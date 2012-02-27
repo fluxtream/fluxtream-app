7512618254cfe10d8da8ad4d88e078cdebaf6428
Candide Kemmler <ckemmler@gmail.com>
2012-02-27 20:31:09 +0100

    updating the project to work with the m2e plugin

--- fluxtream-web/src/main/webapp/js/applications/diary/App.js
+++ fluxtream-web/src/main/webapp/js/applications/diary/App.js
@@ -7,7 +7,6 @@ define(["core/Application"], function(Application) {
 	var Diary = {};
 	_.extend(Diary, Application);
 	Diary.name = "Diary";
-	Diary.gotoDate = gotoDate;
 	Diary.initialize = initialize;
 	return Diary;
 	
