0917b5cbeef12640027b070f80d03d4ff5a7fa0c
Candide Kemmler <ckemmler@gmail.com>
2012-03-15 10:24:16 +0100

    checking if the loading animation already exists

--- fluxtream-web/src/main/webapp/js/applications/log/Builder.js
+++ fluxtream-web/src/main/webapp/js/applications/log/Builder.js
@@ -63,8 +63,9 @@ define([], function() {
 				+ capitalizeFirstLetter(name.toLowerCase())
 				+" View</a></li>";
 		}
-		markup += "</ul></div>\
-			<span class=\"loading\"><img src=\"/static/img/loading.gif\"/></span>";
+		markup += "</ul></div>"
+		if (!$(".loading")[0])
+			markup += "<span class=\"loading\"><img src=\"/static/img/loading.gif\"/></span>";
 		$("#calendar-menubar").append(markup);
 		for (name in timeUnits) {
 			$("#time-menu a." + name).click(function(event) {
