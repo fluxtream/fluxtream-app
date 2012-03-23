41aff7db152768a8db95839e51dde7b7a40addce
Eric Park <epark@jeztek.com>
2012-03-23 11:55:01 -0700

    added multiple plots to timeline

--- fluxtream-web/src/main/webapp/js/applications/log/widgets/timeline/TimelineWidget.js
+++ fluxtream-web/src/main/webapp/js/applications/log/widgets/timeline/TimelineWidget.js
@@ -3,7 +3,7 @@ define(
 		function() {
 
 			var viewModel, dateAxis, dataSource;
-			var yAxis;
+		        var yAxis1, yAxis2, yAxis3;
 
 			function render(digest, timeUnit) {
 				require(
@@ -21,24 +21,10 @@ define(
 						});
 			}
 
-			function renderTimelineWidget(data) {
-				viewModel = JSON.parse(data);
-				// Create x-axis
-				
-				dateAxis = new DateAxis("_timeline_dateAxis", "horizontal", {
-					"min" : viewModel["v2"]["x_axis"]["min"],
-					"max" : viewModel["v2"]["x_axis"]["max"]
-				});
-				yAxis = new NumberAxis("_timeline_yAxis", "vertical", {
-					"min" : viewModel["v2"]["y_axes"][0]["min"],
-					"max" : viewModel["v2"]["y_axes"][0]["max"]
-				});
-				var deviceName = viewModel["v2"]["y_axes"][0]["device_name"];
-				var channelName = viewModel["v2"]["y_axes"][0]["channel_name"];
-				dataSource = function(level, offset, success_callback,
-						failure_callback) {
-					var urlPrefix = "/bodytrack/tiles/3/" + deviceName + "."
-							+ channelName + "/";
+
+		  function createTimelineDatasource(userId, deviceName, channelName) {
+		    var urlPrefix = "/bodytrack/tiles/" + userId + "/" + deviceName + "." + channelName + "/";
+		    return function(level, offset, success_callback, failure_callback) {
 					var onerr = function(jqXHR, textStatus, errorThrown) {
 						try {
 							if (failure_callback) {
@@ -63,8 +49,49 @@ define(
 						failure : onerr
 					});
 				};
-				var plot = new DataSeriesPlot(dataSource, dateAxis, yAxis, viewModel.v2.y_axes[0].style);
-				var plotContainer = new PlotContainer("_timeline_pc", [ plot ]);
+		    
+		  }
+			function renderTimelineWidget(data) {
+				viewModel = JSON.parse(data);
+				// Create x-axis
+				
+				dateAxis = new DateAxis("_timeline_dateAxis", "horizontal", {
+					"min" : viewModel["v2"]["x_axis"]["min"],
+					"max" : viewModel["v2"]["x_axis"]["max"]
+				});
+			  	yAxis1 = new NumberAxis("_timeline_yAxis", "vertical", {
+					"min" : viewModel["v2"]["y_axes"][0]["min"],
+					"max" : viewModel["v2"]["y_axes"][0]["max"]
+				});
+
+				yAxis2 = new NumberAxis("_timeline_yAxis", "vertical", {
+					"min" : viewModel["v2"]["y_axes"][1]["min"],
+					"max" : viewModel["v2"]["y_axes"][1]["max"]
+				});
+
+			  	yAxis3 = new NumberAxis("_timeline_yAxis", "vertical", {
+					"min" : viewModel["v2"]["y_axes"][2]["min"],
+					"max" : viewModel["v2"]["y_axes"][2]["max"]
+				});
+
+			  var plot1 = new DatSeriesPlot(createTimelineDatasource(3, viewModel["v2"]["y_axes"][0]["device_name"], viewModel["v2"]["y_axes"][0]["channel_name"]),
+							dateAxis,
+							yAxis1,
+							viewModel["v2"]["y_axes"][0].style);
+
+			  var plot2 = new DatSeriesPlot(createTimelineDatasource(3, viewModel["v2"]["y_axes"][1]["device_name"], viewModel["v2"]["y_axes"][1]["channel_name"]),
+							dateAxis,
+							yAxis2,
+							viewModel["v2"]["y_axes"][1].style);
+
+			  var plot3 = new DatSeriesPlot(createTimelineDatasource(3, viewModel["v2"]["y_axes"][2]["device_name"], viewModel["v2"]["y_axes"][2]["channel_name"]),
+							dateAxis,
+							yAxis3,
+							viewModel["v2"]["y_axes"][2].style);
+
+			  var plotContainer1 = new PlotContainer("_timeline_pc_1", [plot1]);
+			  var plotContainer2 = new PlotContainer("_timeline_pc_2", [plot2]);
+			  var plotContainer3 = new PlotContainer("_timeline_pc_3", [plot3]);
 			}
 
 			var widget = {};
