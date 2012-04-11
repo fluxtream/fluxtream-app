f9b2c26bf0a0630c2152fa19373ece5ea66a50d4
Anne Wright <anne@liad.local>
2012-04-11 07:17:17 -0400

    Anne added images for bodymedia and bodytrack for the admin interface, fleshed out BodymediaUpdater.java just a bit, and made changes that she expected would make the bodymedia connector appear correctly in the add dialog.  The lattermost doesn't seem to have fully worked, but she doesn't understand why.

--- fluxtream-connectors/src/main/java/com/fluxtream/connectors/bodymedia/BodymediaUpdater.java
+++ fluxtream-connectors/src/main/java/com/fluxtream/connectors/bodymedia/BodymediaUpdater.java
@@ -1,10 +1,24 @@
 package com.fluxtream.connectors.bodymedia;
 
+import com.fluxtream.connectors.ObjectType;
 import com.fluxtream.connectors.annotations.Updater;
+import com.fluxtream.connectors.updaters.AbstractUpdater;
+import com.fluxtream.connectors.updaters.UpdateInfo;
 
 @Updater(prettyName = "BodyMedia", value = 88, objectTypes = {
 		BodymediaBurnFacet.class, BodymediaSleepFacet.class,
 		BodymediaStepsFacet.class }, hasFacets = true)
-public class BodymediaUpdater {
+public class BodymediaUpdater extends AbstractUpdater {
+	
+	public BodymediaUpdater() {
+		super();
+	}
 
+	public void updateConnectorDataHistory(UpdateInfo updateInfo)
+			throws Exception {
+		ObjectType burnOT = ObjectType.getObjectType(connector(), "burn");
+	}
+	
+	public void updateConnectorData(UpdateInfo updateInfo) throws Exception {
+	}
 }
