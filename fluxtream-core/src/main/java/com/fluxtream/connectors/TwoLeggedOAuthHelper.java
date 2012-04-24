c2edce88638eef8308b274ceab08c74da8650b38
Candide Kemmler <ckemmler@gmail.com>
2012-04-24 16:22:16 -0400

    added intellij project descriptors

--- fluxtream-core/src/main/java/com/fluxtream/connectors/TwoLeggedOAuthHelper.java
+++ fluxtream-core/src/main/java/com/fluxtream/connectors/TwoLeggedOAuthHelper.java
@@ -16,65 +16,65 @@ import com.fluxtream.connectors.updaters.RateLimitReachedException;
 
 @Component
 public class TwoLeggedOAuthHelper extends ApiClientSupport {
-	
-	public final String makeRestCall(Connector connector, long guestId,
-			String accessToken, String tokenSecret, Map<String,String> additionalParameters,
-			int objectTypes, String urlString) throws RateLimitReachedException {
 
-		if (hasReachedRateLimit(connector, guestId))
-			throw new RateLimitReachedException();
+    public final String makeRestCall(Connector connector, long guestId,
+                                     String accessToken, String tokenSecret, Map<String, String> additionalParameters,
+                                     int objectTypes, String urlString) throws RateLimitReachedException {
 
-		try {
-			long then = System.currentTimeMillis();
-			URL url = new URL(urlString);
-			HttpURLConnection request = (HttpURLConnection) url.openConnection();
-			
-			OAuthConsumer consumer = new DefaultOAuthConsumer(
-					"", "");
-			if (additionalParameters!=null && additionalParameters.size()>0)
-				addAdditionalParameters(consumer, additionalParameters);
-	
-			consumer.setTokenWithSecret(
-					accessToken,
-					tokenSecret);
-			
-			// sign the request (consumer is a Signpost DefaultOAuthConsumer)
-			try {
-				consumer.sign(request);
-			} catch (Exception e) {
-				throw new RuntimeException("OAuth exception: " + e.getMessage());
-			}
-			request.connect();
-			if (request.getResponseCode() == 200) {
-				String response = IOUtils.toString(request.getInputStream());
-				connectorUpdateService.addApiUpdate(guestId, connector,
-						objectTypes, then, System.currentTimeMillis() - then,
-						urlString, true);
-				// logger.info(apiKey.getGuestId(), "REST call success: " +
-				// urlString);
-				return response;
-			} else {
-				connectorUpdateService.addApiUpdate(guestId, connector,
-						objectTypes, then, System.currentTimeMillis() - then,
-						urlString, false);
-				throw new RuntimeException(
-						"Could not make REST call, got response code: "
-								+ request.getResponseCode() + ", message: "
-								+ request.getResponseMessage() + "\n+REST url: "
-								+ urlString);
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("IOException trying to make rest call: " + e.getMessage());
-		}
-	}
+        if (hasReachedRateLimit(connector, guestId))
+            throw new RateLimitReachedException();
 
-	private void addAdditionalParameters(OAuthConsumer consumer,
-			Map<String, String> additionalParameters) {
-		for (String additionalParameterName : additionalParameters.keySet()) {
-			HttpParameters additionalParameter = new HttpParameters();
-			additionalParameter.put(additionalParameterName, additionalParameters.get(additionalParameterName));
-			consumer.setAdditionalParameters(additionalParameter);
-		}
-	}
+        try {
+            long then = System.currentTimeMillis();
+            URL url = new URL(urlString);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+
+            OAuthConsumer consumer = new DefaultOAuthConsumer(
+                    accessToken, tokenSecret);
+            if (additionalParameters != null && additionalParameters.size() > 0)
+                addAdditionalParameters(consumer, additionalParameters);
+
+            consumer.setTokenWithSecret(
+                    "",
+                    "");
+
+            // sign the request (consumer is a Signpost DefaultOAuthConsumer)
+            try {
+                consumer.sign(request);
+            } catch (Exception e) {
+                throw new RuntimeException("OAuth exception: " + e.getMessage());
+            }
+            request.connect();
+            if (request.getResponseCode() == 200) {
+                String response = IOUtils.toString(request.getInputStream());
+                connectorUpdateService.addApiUpdate(guestId, connector,
+                        objectTypes, then, System.currentTimeMillis() - then,
+                        urlString, true);
+                // logger.info(apiKey.getGuestId(), "REST call success: " +
+                // urlString);
+                return response;
+            } else {
+                connectorUpdateService.addApiUpdate(guestId, connector,
+                        objectTypes, then, System.currentTimeMillis() - then,
+                        urlString, false);
+                throw new RuntimeException(
+                        "Could not make REST call, got response code: "
+                                + request.getResponseCode() + ", message: "
+                                + request.getResponseMessage() + "\n+REST url: "
+                                + urlString);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("IOException trying to make rest call: " + e.getMessage());
+        }
+    }
+
+    private void addAdditionalParameters(OAuthConsumer consumer,
+                                         Map<String, String> additionalParameters) {
+        for (String additionalParameterName : additionalParameters.keySet()) {
+            HttpParameters additionalParameter = new HttpParameters();
+            additionalParameter.put(additionalParameterName, additionalParameters.get(additionalParameterName));
+            consumer.setAdditionalParameters(additionalParameter);
+        }
+    }
 
 }
