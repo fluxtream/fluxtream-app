e4667302e7093039004074c1855c8fd40d2023f7
Chris Bartley <bartley@cmu.edu>
2012-05-02 12:48:56 -0400

    Fixed issue with CalendarEventEntry instances having empty list of times.

--- fluxtream-connectors/src/main/java/com/fluxtream/connectors/google_calendar/GoogleCalendarUpdater.java
+++ fluxtream-connectors/src/main/java/com/fluxtream/connectors/google_calendar/GoogleCalendarUpdater.java
@@ -2,9 +2,6 @@ package com.fluxtream.connectors.google_calendar;
 
 import java.net.URL;
 import java.util.List;
-
-import org.springframework.stereotype.Component;
-
 import com.fluxtream.connectors.Connector.UpdateStrategyType;
 import com.fluxtream.connectors.annotations.JsonFacetCollection;
 import com.fluxtream.connectors.annotations.Updater;
@@ -16,77 +13,95 @@ import com.google.gdata.client.calendar.CalendarService;
 import com.google.gdata.data.DateTime;
 import com.google.gdata.data.calendar.CalendarEventEntry;
 import com.google.gdata.data.calendar.CalendarEventFeed;
+import com.google.gdata.data.extensions.When;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
 
 @Component
-@Updater(prettyName = "Google Calendar", value = 0, updateStrategyType = UpdateStrategyType.ALWAYS_UPDATE, objectTypes = { GoogleCalendarEntryFacet.class })
+@Updater(prettyName = "Google Calendar", value = 0, updateStrategyType = UpdateStrategyType.ALWAYS_UPDATE, objectTypes = {GoogleCalendarEntryFacet.class})
 @JsonFacetCollection(GoogleCalendarFacetVOCollection.class)
 public class GoogleCalendarUpdater extends AbstractGoogleOAuthUpdater {
 
-	public GoogleCalendarUpdater() {
-		super();
-	}
+    private static final Logger LOG = Logger.getLogger(GoogleCalendarUpdater.class);
 
-	@Override
-	protected void updateConnectorDataHistory(UpdateInfo updateInfo)
-			throws Exception {
-		loadHistory(updateInfo, 0, System.currentTimeMillis());
-	}
+    public GoogleCalendarUpdater() {
+        super();
+    }
 
-	@Override
-	public void updateConnectorData(UpdateInfo updateInfo) throws Exception {
-		loadHistory(updateInfo, updateInfo.timeInterval.start,
-				updateInfo.timeInterval.end);
-	}
+    @Override
+    protected void updateConnectorDataHistory(UpdateInfo updateInfo) throws Exception {
+        loadHistory(updateInfo, 0, System.currentTimeMillis());
+    }
 
-	private void loadHistory(UpdateInfo updateInfo, long from, long to)
-			throws Exception {
-		long then = System.currentTimeMillis();
-		String requestUrl = "request url not set yet";
-		List<CalendarEventEntry> entries = null;
-		try {
-			URL feedUrl = new URL(
-					"https://www.google.com/calendar/feeds/default/private/full");
-			CalendarQuery myQuery = new CalendarQuery(feedUrl);
-			myQuery.setMaxResults(200);
-			myQuery.setMinimumStartTime(DateTime.parseDateTime(dateFormat
-					.print(from)));
-			myQuery.setMaximumStartTime(DateTime.parseDateTime(dateFormat
-					.print(to)));
+    @Override
+    public void updateConnectorData(UpdateInfo updateInfo) throws Exception {
+        loadHistory(updateInfo,
+                    updateInfo.timeInterval.start,
+                    updateInfo.timeInterval.end);
+    }
 
-			requestUrl = myQuery.getFeedUrl().getQuery();
+    private void loadHistory(UpdateInfo updateInfo, long from, long to) throws Exception {
+        long then = System.currentTimeMillis();
+        String requestUrl = "request url not set yet";
+        List<CalendarEventEntry> entries;
+        try {
+            URL feedUrl = new URL("https://www.google.com/calendar/feeds/default/private/full");
+            CalendarQuery myQuery = new CalendarQuery(feedUrl);
+            myQuery.setStringCustomParameter("singleevents", "true");   // See http://stackoverflow.com/a/3648754
+            myQuery.setMaxResults(200);
+            myQuery.setMinimumStartTime(DateTime.parseDateTime(dateFormat.print(from)));
+            myQuery.setMaximumStartTime(DateTime.parseDateTime(dateFormat.print(to)));
 
-			CalendarService myService = new CalendarService("fluxtream");
-			myService.setOAuthCredentials(
-					getOAuthParameters(updateInfo.apiKey),
-					new OAuthHmacSha1Signer());
+            requestUrl = myQuery.getFeedUrl().getQuery();
 
-			CalendarEventFeed resultFeed = myService.query(myQuery,
-					CalendarEventFeed.class);
+            CalendarService myService = new CalendarService("fluxtream");
+            myService.setOAuthCredentials(getOAuthParameters(updateInfo.apiKey),
+                                          new OAuthHmacSha1Signer());
 
-			entries = resultFeed.getEntries();
-		} catch (Exception e) {
-			countFailedApiCall(updateInfo.apiKey.getGuestId(),
-					updateInfo.objectTypes, then, requestUrl);
-			throw e;
-		}
+            CalendarEventFeed resultFeed = myService.query(myQuery,
+                                                           CalendarEventFeed.class);
 
-		countSuccessfulApiCall(updateInfo.apiKey.getGuestId(),
-				updateInfo.objectTypes, then, requestUrl);
+            entries = resultFeed.getEntries();
+        }
+        catch (Exception e) {
+            countFailedApiCall(updateInfo.apiKey.getGuestId(),
+                               updateInfo.objectTypes,
+                               then,
+                               requestUrl);
+            throw e;
+        }
 
-		if (entries != null) {
-			for (CalendarEventEntry calendarEventEntry : entries) {
-				GoogleCalendarEntryFacet payload = new GoogleCalendarEntryFacet(
-						calendarEventEntry);
-				payload.start = calendarEventEntry.getTimes().get(0)
-						.getStartTime().getValue();
-				payload.end = calendarEventEntry.getTimes().get(0).getEndTime()
-						.getValue();
-				apiDataService.cacheApiDataObject(updateInfo, -1, -1,
-						payload);
-			}
-		} else
-			throw new Exception(
-					"null entries when loading google calendar history");
-	}
+        countSuccessfulApiCall(updateInfo.apiKey.getGuestId(),
+                               updateInfo.objectTypes,
+                               then,
+                               requestUrl);
 
+        if (entries != null) {
+            for (CalendarEventEntry calendarEventEntry : entries) {
+                final List<When> times = calendarEventEntry.getTimes();
+                if (times != null) {
+                    if (!times.isEmpty()) {
+                        GoogleCalendarEntryFacet payload = new GoogleCalendarEntryFacet(calendarEventEntry);
+                        payload.start = times.get(0).getStartTime().getValue();
+                        payload.end = times.get(0).getEndTime().getValue();
+                        apiDataService.cacheApiDataObject(updateInfo, -1, -1, payload);
+                    }
+                    else {
+                        if (LOG.isEnabledFor(Level.ERROR)) {
+                            LOG.error("GoogleCalendarUpdater.loadHistory(): CalendarEventEntry times is empty for event [" + calendarEventEntry.getTitle().getPlainText() + "]");
+                        }
+                    }
+                }
+                else {
+                    if (LOG.isEnabledFor(Level.ERROR)) {
+                        LOG.error("GoogleCalendarUpdater.loadHistory(): CalendarEventEntry times is null for event [" + calendarEventEntry.getTitle().getPlainText());
+                    }
+                }
+            }
+        }
+        else {
+            throw new Exception("null entries when loading google calendar history");
+        }
+    }
 }
