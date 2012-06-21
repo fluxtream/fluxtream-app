befb425d6549d3f94568d2207245e13dcdd55304
justin <justin@Tadpole.(none)>
2012-06-21 11:48:34 -0400

    Fixed a bug in MapUtils where highlighted sections of the map gps line would never vanish

--- fluxtream-web/src/main/webapp/js/applications/calendar/tabs/map/MapUtils.js
+++ fluxtream-web/src/main/webapp/js/applications/calendar/tabs/map/MapUtils.js
@@ -305,7 +305,7 @@ define(["applications/calendar/tabs/map/MapConfig"], function(Config) {
             return;
         }
         map.gpsLine.setOptions({strokeColor: "grey"});
-        highlightSection = map.createPolyLineSegment(start, end, {strokeColor:"black", zIndex: 99});
+        map.highlightSection = map.createPolyLineSegment(start, end, {strokeColor:"black", zIndex: 99});
     }
 
     function getFirstIndexAfter(map, time){
