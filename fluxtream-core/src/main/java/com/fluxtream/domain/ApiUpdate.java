52d2a278e5652791189c82e2b0f4fb7c447b4e36
justin <justin@Tadpole.(none)>
2012-06-28 14:30:11 -0400

    fixed compile error

--- fluxtream-core/src/main/java/com/fluxtream/domain/ApiUpdate.java
+++ fluxtream-core/src/main/java/com/fluxtream/domain/ApiUpdate.java
@@ -30,8 +30,7 @@ import org.hibernate.annotations.Type;
 	@NamedQuery( name="apiUpdates.last.successful.byApiAndObjectTypes",
 		query="SELECT updt FROM ApiUpdates updt WHERE updt.guestId=? and updt.api=? and updt.objectTypes=? and updt.success=true ORDER BY updt.ts DESC"),
     @NamedQuery( name="apiUpdates.lastSync",
-        query="SELECT updt FROM ApiUpdates updt WHERE updt.guestId=? and updt.api=? and updt.objectTypes=? and updt.success=true and lastSync<>0 ORDER BY updt.lastSync DESC LIMIT 10"),
-})
+        query="SELECT updt FROM ApiUpdates updt WHERE updt.guestId=? and updt.api=? and updt.objectTypes=? and updt.success=true and lastSync<>0 ORDER BY updt.lastSync DESC LIMIT 10")})
 public class ApiUpdate extends AbstractEntity {
 
 	@Index(name="guestId")
