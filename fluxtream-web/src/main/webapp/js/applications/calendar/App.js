0ecb5db449a5d04b725e85dd4c8a9ed97d09c9aa
justin <justin@Tadpole.(none)>
2012-06-28 15:44:17 -0400

    added twitter parsing support. twitter facets now have their text parsed for twitter usernames and twitter hash tags with links to respective profiles and search fields

--- fluxtream-web/src/main/webapp/js/applications/calendar/App.js
+++ fluxtream-web/src/main/webapp/js/applications/calendar/App.js
@@ -340,11 +340,83 @@ define(["core/Application", "core/FlxState", "applications/calendar/Builder", "l
                 case "imgUrls":
                     params["imgUrl"] = data[member][0];
                     break;
+                case "description":
+                    if (data.type == "twitter-dm" || data.type == "twitter-mention" || data.type == "twitter-tweet"){
+                        params[member] = parseTwitter(data[member]);
+                    }
+                    else{
+                        params[member] = data[member];
+                    }
+                    break;
                 default:
                     params[member] = data[member];
             }
         }
         return digest.detailsTemplates[data.type].render(params);
+    }
+
+    function parseTwitter(text){
+        var indexAt = 0;
+        var result = "";
+        while (indexAt != text.length){
+            var foundHashAt = text.indexOf("#",indexAt);
+            var foundAtAt = text.indexOf("@",indexAt);
+            if (foundHashAt == -1 && foundAtAt == -1){
+                result += text.substring(indexAt);
+                break;
+            }
+            else if (foundAtAt != -1 && (foundAtAt < foundHashAt || foundHashAt == -1)){
+                result += text.substring(indexAt,foundAtAt);
+                var foundSpaceAt = text.indexOf(" ",foundAtAt);
+                if (foundAtAt == text.length - 1 || foundSpaceAt == foundAtAt + 1 || foundHashAt == foundAtAt + 1){
+                    result += "@";
+                    indexAt = foundAtAt + 1;
+                }
+                else if (foundSpaceAt == -1 && foundHashAt == -1){
+                    var linkText = text.substring(foundAtAt);
+                    var userName = linkText.substring(1);
+                    result += "<a href='https://twitter.com/" + encodeURIComponent(userName) + "'>" + linkText + "</a>";
+                    break;
+                }
+                else if (foundSpaceAt != -1 && (foundSpaceAt < foundHashAt || foundHashAt == -1)){
+                    var linkText = text.substring(foundAtAt,foundSpaceAt);
+                    var userName = linkText.substring(1);
+                    result += "<a href='https://twitter.com/" + encodeURIComponent(userName) + "'>" + linkText + "</a>";
+                    indexAt = foundSpaceAt;
+                }
+                else{
+                    var linkText = text.substring(foundAtAt,foundHashAt);
+                    var userName = linkTextt.substring(1);
+                    result += "<a href='https://twitter.com/" + encodeURIComponent(userName) + "'>" + linkText + "</a>";
+                    indexAt = foundHashAt;
+                }
+            }
+            else{
+                result += text.substring(indexAt,foundHashAt);
+                var foundSpaceAt = text.indexOf(" ",foundHashAt);
+                if (foundHashAt == text.length - 1 || foundSpaceAt == foundHashAt + 1 || foundAtAt == foundHashAt + 1){
+                    result += "#";
+                    indexAt = foundHashAt + 1;
+                }
+                else if (foundSpaceAt == -1 && foundAtAt == -1){
+                    var linkText = text.substring(foundHashAt);
+                    result += "<a href='https://twitter.com/search/" + encodeURIComponent(linkText) + "'>" + linkText + "</a>";
+                    break;
+                }
+                else if (foundSpaceAt != -1 && (foundSpaceAt < foundAtAt || foundAtAt == -1)){
+                    var linkText = text.substring(foundHashAt,foundSpaceAt);
+                    result += "<a href='https://twitter.com/search/" + encodeURIComponent(linkText) + "'>" + linkText + "</a>";
+                    indexAt = foundSpaceAt;
+                }
+                else{
+                    var linkText = text.substring(foundHashAt,foundAtAt);
+                    result += "<a href='https://twitter.com/search/" + encodeURIComponent(linkText) + "'>" + linkText + "</a>";
+                    indexAt = foundAtAt;
+                }
+            }
+        }
+        return result;
+
     }
 
     function handleCityInfo(digestInfo) {
