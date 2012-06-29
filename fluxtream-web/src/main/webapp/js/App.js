2b7cd3720db55437b300526a977f6dd6a4f1e94a
Justin Loutsenhizer <nano351@gmail.com>
2012-06-29 16:18:12 -0400

    added more detailed logging and error handling for missing facet configurations

--- fluxtream-web/src/main/webapp/js/App.js
+++ fluxtream-web/src/main/webapp/js/App.js
@@ -232,15 +232,20 @@ define(
         App.getFacetConfig = function(facetName){
             var config = ConnectorConfig[App.getFacetConnector(facetName)];
             if (config == null){
-                console.log("WARNING: No config found for connector: " + connectorName);
+                console.log("WARNING: No config found for Connector: " + App.getFacetConnector(facetName));
                 config = ConnectorConfig.default;
             }
             var finalConfig = $.extend({},config);
             finalConfig.facets = null;
-            var facet = config.facets[App.getFacetObjectType(facetName)];
-            if (facet != null){
-                for (var member in facet){
-                    finalConfig[member] = facet[member];
+            if (config.facets == null || config.facets[App.getFacetObjectType(facetName)] == null){
+                console.log("WARNING: No config found for Facet: " + facetName);
+            }
+            else{
+                var facet = config.facets[App.getFacetObjectType(facetName)];
+                if (facet != null){
+                    for (var member in facet){
+                        finalConfig[member] = facet[member];
+                    }
                 }
             }
             return finalConfig;
