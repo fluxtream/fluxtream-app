c099719e5461bdad8483b049356222c14d39a55b
Justin Loutsenhizer <nano351@gmail.com>
2012-07-02 13:21:58 -0400

    added uri matching to twitter parser

--- fluxtream-web/src/main/webapp/js/applications/calendar/App.js
+++ fluxtream-web/src/main/webapp/js/applications/calendar/App.js
@@ -354,21 +354,50 @@ define(["core/Application", "core/FlxState", "applications/calendar/Builder", "l
         return digest.detailsTemplates[data.type].render(params);
     }
 
+    var topLevelDomains = ["aero","asia","biz","cat","com","coop","info", "int", "jobs", "mobi",
+                           "museum", "name", "net", "org", "pro", "tel", "travel", "xxx", "edu",
+                           "edu", "gov", "mil", "ac", "ad", "ae", "af", "ag", "ai", "al", "am",
+                           "am", "an", "aq", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb",
+                           "be", "bf", "bg", "bh", "bi", "bj", "bm", "bn" , "bo", "br", "bs", "bt",
+                           "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck",
+                           "cl", "cm", "cn", "co", "cr", "cs", "cu", "cv", "cx", "cy", "cz", "dd",
+                           "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et",
+                           "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg",
+                           "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy",
+                           "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq",
+                           "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km",
+                           "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls",
+                           "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mk", "ml", "mm",
+                           "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz",
+                           "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om",
+                           "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw",
+                           "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg",
+                           "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st", "su", "sv",
+                           "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to",
+                           "tp", "tr", "tt", "tv", "tw", "tz", "ua", "uk", "us", "uy", "uz", "va", "vc",
+                           "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "yu", "za", "zm", "zw"]
+
+    var uriRegex = "^(http://|ftp://)?[A-z0-9\\-.]+.(" + topLevelDomains.join("|") + ")(/[/A-z0-9#@_\\-?.]+\\b|\\b)";
+    var twitterNameRegex = "@[A-Za-z0-9_]+";
+    var hashtagRegex = "#[A-Za-z_]+";
+    var twitterParserRegexp = new RegExp("(" + uriRegex + "|" + twitterNameRegex + "|" + hashtagRegex + ")","ig");
+
     function parseTwitter(text){
-        var regexp = /(@[A-Za-z0-9_]+|#[A-Za-z_]+)/ig
         var ret = "";
         var oldIndex = 0;
-        text.replace(regexp, function(match, matchPart1, index, fullString){
+        text.replace(twitterParserRegexp, function(match, $2, $3, $4, $5,index,fullString){
             ret += fullString.substring(oldIndex,index);
             oldIndex = index + match.length;
             switch (match.charAt(0)){
-                case "#":
+                case "#"://hashtag
                     ret += "<a href='https://twitter.com/search/" + encodeURIComponent(match) + "'>" + match + "</a>";
                     break;
-                case "@":
+                case "@"://username
                     ret += "<a href='https://twitter.com/" + encodeURIComponent(match.substring(1)) + "'>" + match + "</a>";
                     break;
-
+                default://uri
+                    ret += "<a href='" + match + "'>" + match + "</a>";
+                    break;
             }
         });
         ret += text.substring(oldIndex);
