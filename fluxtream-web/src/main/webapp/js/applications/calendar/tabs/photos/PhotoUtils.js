e6230c2584a1aeaa79314d5b12e101766809d920
Justin Loutsenhizer <nano351@gmail.com>
2012-07-09 17:31:58 -0400

    fixed a crash when no photos existed for creating carousel with

--- fluxtream-web/src/main/webapp/js/applications/calendar/tabs/photos/PhotoUtils.js
+++ fluxtream-web/src/main/webapp/js/applications/calendar/tabs/photos/PhotoUtils.js
@@ -7,10 +7,12 @@ define([],function(){
     });
 
     function getCarouselHTML(digest){
+        if (digest.cachedData["picasa-photo"] == null)
+            return null;
         var data = digest.cachedData["picasa-photo"];
         var data = [];
         for (var i = 0; i < digest.cachedData["picasa-photo"].length; i++){
-            data[i] = digest.cachedData["picasa-photo"][i]
+            data[i] = digest.cachedData["picasa-photo"][i];
             data[i].active = i == 0;
             data[i].id = i;
         }
