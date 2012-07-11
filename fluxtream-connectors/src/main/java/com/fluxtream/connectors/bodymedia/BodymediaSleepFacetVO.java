323c6e250d5e5da49c03c654e217c40a4b416c1d
Candide Kemmler <ckemmler@gmail.com>
2012-07-11 17:49:36 -0400

    making other bodymedia facets visible in list/clock... views

---
+++ fluxtream-connectors/src/main/java/com/fluxtream/connectors/bodymedia/BodymediaSleepFacetVO.java
@@ -0,0 +1,24 @@
+package com.fluxtream.connectors.bodymedia;
+
+import javax.persistence.Lob;
+import com.fluxtream.TimeInterval;
+import com.fluxtream.connectors.vos.AbstractFacetVO;
+import com.fluxtream.domain.GuestSettings;
+import com.fluxtream.mvc.models.DurationModel;
+
+public class BodymediaSleepFacetVO extends AbstractFacetVO<BodymediaSleepFacet>{
+
+    double efficiency;
+    DurationModel totalLying;
+    DurationModel totalSleeping;
+    String sleepJson;
+
+    @Override
+    protected void fromFacet(final BodymediaSleepFacet facet, final TimeInterval timeInterval, final GuestSettings settings) {
+        this.efficiency = facet.efficiency;
+        this.totalSleeping = new DurationModel(facet.totalSleeping*60);
+        this.totalLying = new DurationModel(facet.totalLying*60);
+        this.sleepJson = facet.sleepJson;
+    }
+
+}
