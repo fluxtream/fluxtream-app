5f3713562bf5477af03f45e7d099fbc19dcaa309
Justin Loutsenhizer <nano351@gmail.com>
2012-07-16 14:22:31 -0400

    fixed steps history to reload the channels list when changing channels

--- fluxtream-web/src/main/webapp/widgets/stepsHistory/stepsHistory.js
+++ fluxtream-web/src/main/webapp/widgets/stepsHistory/stepsHistory.js
@@ -79,15 +79,22 @@ define(["core/DashboardWidget", "core/GrapherComponent", "applications/calendar/
     StepsHistory.bindWidgetSettings = function(widgetSettings){
         bindStyleEditor(widgetSettings.style);
         BodyTrack.SOURCES.getAvailableList(function (sources){
+
+            $("#deviceSelector").change(function(){
+                $("#channelSelector")[0] = 0;
+                $("#channelSelector").html("");
+                var i = $("#deviceSelector")[0].selectedIndex;
+                for (var j = 0; j < sources[i].channels.length; j++){
+                    $("#channelSelector").append("<option>" + sources[i].channels[j].name + "</option>");
+                    if (sources[i].channels[j].name == widgetSettings.channelName)
+                        $("#channelSelector")[0].selectedIndex = j;
+                }
+            });
             for (var i = 0; i < sources.length; i++){
                 $("#deviceSelector").append("<option>" + sources[i].name + "</option>");
                 if (sources[i].name == widgetSettings.deviceName){
                     $("#deviceSelector")[0].selectedIndex = i;
-                    for (var j = 0; j < sources[i].channels.length; j++){
-                        $("#channelSelector").append("<option>" + sources[i].channels[j].name + "</option>");
-                        if (sources[i].channels[j].name == widgetSettings.channelName)
-                            $("#channelSelector")[0].selectedIndex = j;
-                    }
+                    $("#deviceSelector").change();
                 }
             }
             $("#deviceSelector").enable();
