fb16dd1e72f6a0420652a5278999ef3d4c08c470
Justin Loutsenhizer <nano351@gmail.com>
2012-07-23 16:27:45 -0400

    changed BodyTrack app's javascript object name

--- fluxtream-web/src/main/webapp/js/applications/bodytrack/App.js
+++ fluxtream-web/src/main/webapp/js/applications/bodytrack/App.js
@@ -1,25 +1,25 @@
 define(["core/Application", "core/FlxState"], function(Application, FlxState) {
 
-    var Pinboard = new Application("bodytrack", "Candide Kemmler", "icon-bookmark");
+    var BodyTrack = new Application("bodytrack", "Candide Kemmler", "icon-bookmark");
 
-    Pinboard.initialize = function () {
+    BodyTrack.initialize = function () {
         FlxState.router.route(/^app\/bodytrack(\/?)(.*?)$/, "", function() {
-            Pinboard.render("");
+            BodyTrack.render("");
         });
     };
 
-    Pinboard.saveState = function() {
+    BodyTrack.saveState = function() {
     };
 
-    Pinboard.renderState = function(state) {
+    BodyTrack.renderState = function(state) {
         if (state===null) state = "";
         else state = "/" + state;
         FlxState.router.navigate("app/bodytrack" + state);
         FlxState.saveState("pinboard", state);
     };
 
-    Pinboard.setup = function() {
+    BodyTrack.setup = function() {
     };
 
-    return Pinboard;
+    return BodyTrack;
 });
