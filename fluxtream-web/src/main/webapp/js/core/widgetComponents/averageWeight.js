37c0976299ff2e5dfce9d990f1af2b130f0775c3
jloutsenhizer <nano351@gmail.com>
2012-08-02 14:46:50 -0400

    -began ground work for additional widget components -added averageSteps widget component -added averageWeight widget component -averageSteps widget now uses averageSteps widget component -averageWeight widget now uses averageWeight widget comonent -on day view Fitbit.steps and BodyMedia.totalSteps use averageSteps widget component instead of grapherComponent widget component

---
+++ fluxtream-web/src/main/webapp/js/core/widgetComponents/averageWeight.js
@@ -0,0 +1,28 @@
+define([],function(){
+
+    var AverageWeightComponent = function(parentWidget,parentElement,dataArray,weightParamName){
+        this.parent = parentWidget;
+        App.loadMustacheTemplate("core/widgetComponents/widgetComponentTemplates.html","averageWeight",function(template){
+            var weight = 0;
+            var averageWeight = 0;
+            var measures = 0;
+            for (var i=0; i<dataArray.length; i++) {
+                if (dataArray[i][weightParamName]>0) {
+                    measures++;
+                    averageWeight+=dataArray[i][weightParamName];
+                }
+            }
+            weight = Math.round((averageWeight/measures)*100)/100;
+            var unit = parentWidget.digest.settings.weightMeasureUnit;
+            if (unit==="SI")
+                unit = "kg";
+            else unit = unit.toLowerCase();
+            $(parentElement).append(
+                template.render({"manifest" : parentWidget.manifest, "weight" : weight>0?weight:null, "unit" : unit, "noMeasure" : weight===0})
+            );
+        });
+    }
+
+    return AverageWeightComponent;
+
+});
