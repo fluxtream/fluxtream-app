c89bac057f345c3ae9fb8f10d7157bfefa1156a3
Candide Kemmler <ckemmler@gmail.com>
2012-09-12 20:37:57 -0700

    fixed city/state mismatch bug

--- fluxtream-core/src/main/java/com/fluxtream/domain/metadata/City.java
+++ fluxtream-core/src/main/java/com/fluxtream/domain/metadata/City.java
@@ -9,6 +9,9 @@ import org.hibernate.annotations.Index;
 
 @Entity(name="cities1000")
 @NamedQueries ( {
+    @NamedQuery( name="city.byNameStateAndCountryCode",
+                 query="SELECT city from cities1000 city WHERE city.geo_name=? AND " +
+                       "city.state=? AND city.geo_country_code=?"),
 	@NamedQuery( name="city.byNameAndCountryCode",
 			query="SELECT city from cities1000 city WHERE city.geo_name=? AND city.geo_country_code=?")
 })
