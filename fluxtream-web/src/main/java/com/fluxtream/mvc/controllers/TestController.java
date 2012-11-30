51c4dd2ecf7674a7bb7fac199c22f7db7c76fec2
Evan Savage <savage.evan@gmail.com>
2012-11-30 10:57:06 -0800

    PhantomJS integration now works
    Previously, phantomjs would spew out a bunch of errors like
    
    "ReferenceError: can't find variable jasmine"
    
    Digging into this, I found (by adding console.log(page.url) to the
    jasmine phantomjs runner script) that it wasn't loading the test page,
    but was instead being redirected to /welcome.
    
    This diff adds /test/** to the set of paths outside the security wall,
    which resolves this issue.

--- fluxtream-web/src/main/java/com/fluxtream/mvc/controllers/TestController.java
+++ fluxtream-web/src/main/java/com/fluxtream/mvc/controllers/TestController.java
@@ -2,12 +2,8 @@ package com.fluxtream.mvc.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 import com.fluxtream.Configuration;
-import com.fluxtream.auth.AuthHelper;
-import com.fluxtream.domain.Guest;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
