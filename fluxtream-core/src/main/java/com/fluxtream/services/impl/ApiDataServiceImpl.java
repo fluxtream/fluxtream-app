fluxtream/fluxtream-app

commit 2c8539c76acbbcd2576458c9ea0144d910a5d836

runkeeper history update done

fluxtream-core/src/main/java/com/fluxtream/services/impl/ApiDataServiceImpl.java
@@ -552,23 +552,9 @@ public class ApiDataServiceImpl implements ApiDataService {
 
     @Override
     @Transactional(readOnly = false)
-    public void addGuestLocation(final long guestId, LocationFacet locationResource,
-                                 final LocationFacet.Source source) {
-        LocationFacet payload = new LocationFacet();
-        payload.source = source;
-        payload.latitude = locationResource.latitude;
-        payload.longitude = locationResource.longitude;
-        payload.start = locationResource.timestampMs;
-        payload.end = locationResource.timestampMs;
-        payload.api = Connector.getConnector("google_latitude").value();
-        payload.guestId = guestId;
-        payload.timestampMs = locationResource.timestampMs;
-        payload.accuracy = locationResource.accuracy;
-        payload.timeUpdated = System.currentTimeMillis();
-
+    public void addGuestLocation(final long guestId, LocationFacet locationResource) {
         updateDayMetadata(guestId, locationResource.timestampMs, locationResource.latitude, locationResource.longitude);
-
-        em.persist(payload);
+        em.persist(locationResource);
     }
 
     @Transactional(readOnly = false)

fluxtream-connectors/src/main/java/com/fluxtream/connectors/evernote/EvernoteUpdater.java
@@ -3,11 +3,13 @@ package com.fluxtream.connectors.evernote;
 import com.fluxtream.connectors.annotations.Updater;
 import com.fluxtream.connectors.updaters.AbstractUpdater;
 import com.fluxtream.connectors.updaters.UpdateInfo;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author Candide Kemmler (candide@fluxtream.com)
  */
+@Component
 @Updater(prettyName = "Evernote", value = 17, objectTypes ={})
 public class EvernoteUpdater extends AbstractUpdater {
 

fluxtream-connectors/src/main/java/com/fluxtream/connectors/google_latitude/GoogleLatitudeUpdater.java
@@ -69,10 +69,10 @@ public class GoogleLatitudeUpdater extends AbstractGoogleOAuthUpdater {
 					continue;
 				locationResource.start = locationResource.timestampMs;
 				locationResource.end = locationResource.timestampMs;
+                locationResource.source = LocationFacet.Source.GOOGLE_LATITUDE;
 
                 apiDataService.addGuestLocation(updateInfo.getGuestId(),
-						locationResource,
-                        LocationFacet.Source.GOOGLE_LATITUDE);
+						locationResource);
 				
 				storedLocations.add(locationResource);
 			}

fluxtream-connectors/src/main/java/com/fluxtream/connectors/runkeeper/DistanceMeasure.java
@@ -1,0 +1,15 @@
+package com.fluxtream.connectors.runkeeper;
+
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author Candide Kemmler (candide@fluxtream.com)
+ */
+@Embeddable
+public class DistanceMeasure {
+
+    public double distance;
+    public double timestamp;
+
+}

fluxtream-connectors/src/main/java/com/fluxtream/connectors/runkeeper/HeartRateMeasure.java
@@ -1,0 +1,16 @@
+package com.fluxtream.connectors.runkeeper;
+
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author Candide Kemmler (candide@fluxtream.com)
+ */
+
+@Embeddable
+public class HeartRateMeasure {
+
+    public double heartRate;
+    public double timestamp;
+
+}

fluxtream-connectors/src/main/java/com/fluxtream/connectors/runkeeper/RunKeeperFitnessActivityExtractor.java
@@ -5,12 +5,16 @@ import java.util.List;
 import java.util.TimeZone;
 import com.fluxtream.ApiData;
 import com.fluxtream.connectors.ObjectType;
+import com.fluxtream.connectors.google_latitude.LocationFacet;
 import com.fluxtream.domain.AbstractFacet;
 import com.fluxtream.facets.extractors.AbstractFacetExtractor;
+import com.fluxtream.services.ApiDataService;
 import com.fluxtream.services.MetadataService;
+import com.google.api.client.util.Key;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.apache.velocity.util.ArrayListWrapper;
+import org.hibernate.annotations.Index;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
@@ -29,6 +33,9 @@ public class RunKeeperFitnessActivityExtractor extends AbstractFacetExtractor {
     @Autowired
     MetadataService metadataService;
 
+    @Autowired
+    ApiDataService apiDataService;
+
     @Override
     public List<AbstractFacet> extractFacets(final ApiData apiData, final ObjectType objectType) throws Exception {
         List<AbstractFacet> facets = new ArrayList<AbstractFacet>();
@@ -36,55 +43,76 @@ public class RunKeeperFitnessActivityExtractor extends AbstractFacetExtractor {
 
         RunKeeperFitnessActivityFacet facet = new RunKeeperFitnessActivityFacet();
 
+        super.extractCommonFacetData(facet, apiData);
+
+        String uri = jsonObject.getString("uri") + "|" + jsonObject.getString("userID");
+
+        boolean startTimeSet = false;
         if (jsonObject.has("path")) {
             final JSONArray path = jsonObject.getJSONArray("path");
-            facet.latitudes = new double[path.size()];
-            facet.longitudes = new double[path.size()];
-            facet.altitudes = new double[path.size()];
-            facet.timestamps = new long[path.size()];
             for (int i=0; i<path.size(); i++) {
                 JSONObject pathElement = path.getJSONObject(i);
-                facet.latitudes[i] = pathElement.getDouble("latitude");
-                facet.longitudes[i] = pathElement.getDouble("longitude");
-                facet.timestamps[i] = pathElement.getLong("timestamp");
-                facet.altitudes[i] = pathElement.getDouble("altitude");
+                LocationFacet locationFacet = new LocationFacet();
+                locationFacet.latitude = (float) pathElement.getDouble("latitude");
+                locationFacet.longitude = (float) pathElement.getDouble("longitude");
+                if (!startTimeSet) {
+                    // we need to know the user's location in order to figure out
+                    // his timezone
+                    final String start_time = jsonObject.getString("start_time");
+                    final TimeZone timeZone = metadataService.getTimeZone(locationFacet.latitude, locationFacet.longitude);
+                    facet.start = timeFormatter.withZone(DateTimeZone.forTimeZone(timeZone)).parseMillis(start_time);
+                    final int duration = jsonObject.getInt("duration");
+                    facet.end = facet.start + duration*1000;
+                    startTimeSet = true;
+                }
+                locationFacet.altitude = (int) pathElement.getDouble("altitude");
+                locationFacet.timestampMs = facet.start + pathElement.getLong("timestamp");
+                locationFacet.source = LocationFacet.Source.RUNKEEPER;
+                locationFacet.uri = uri;
+
+                apiDataService.addGuestLocation(updateInfo.getGuestId(), locationFacet);
             }
         } else {
-            //TODO: abort if we don't have gps data as we are unable to figure out time
+            //TODO: abort elegantly if we don't have gps data as we are unable to figure out time
             //in this case
+            return facets;
         }
-
-        final String start_time = jsonObject.getString("start_time");
-        final TimeZone timeZone = metadataService.getTimeZone(facet.latitudes[0], facet.longitudes[0]);
-        facet.start = timeFormatter.withZone(DateTimeZone.forTimeZone(timeZone)).parseMillis(start_time);
-        final int duration = jsonObject.getInt("duration");
-        facet.end = facet.start + duration*1000;
 
         facet.userID = jsonObject.getString("userID");
         facet.duration = jsonObject.getInt("duration");
         facet.type = jsonObject.getString("type");
         facet.equipment = jsonObject.getString("equipment");
         facet.total_distance = jsonObject.getDouble("total_distance");
-        facet.total_climb = jsonObject.getDouble("total_climb");
+        facet.is_live = jsonObject.getBoolean("is_live");
+        facet.comments = jsonObject.getString("comments");
+        if (jsonObject.has("total_climb"))
+            facet.total_climb = jsonObject.getDouble("total_climb");
 
 
         if (jsonObject.has("distance")) {
             final JSONArray distanceArray = jsonObject.getJSONArray("distance");
-            facet.distance = new double[distanceArray.size()];
-            for (int i=0; i<distanceArray.size(); i++)
-                facet.distance[i] = distanceArray.getDouble(i);
+            DistanceMeasure distanceMeasure = new DistanceMeasure();
+            facet.distance = new ArrayList<DistanceMeasure>(distanceArray.size());
+            for (int i=0; i<distanceArray.size(); i++) {
+                final JSONObject distanceTuple = distanceArray.getJSONObject(i);
+                distanceMeasure.distance = distanceTuple.getDouble("distance");
+                distanceMeasure.timestamp = distanceTuple.getDouble("timestamp");
+                facet.distance.add(distanceMeasure);
+            }
         }
         if (jsonObject.has("heart_rate")) {
             final JSONArray heart_rateArray = jsonObject.getJSONArray("heart_rate");
-            facet.heart_rate = new double[heart_rateArray.size()];
-            for (int i=0; i<heart_rateArray.size(); i++)
-                facet.heart_rate[i] = heart_rateArray.getDouble(i);
+            HeartRateMeasure heartRateMeasure = new HeartRateMeasure();
+            facet.heart_rate = new ArrayList<HeartRateMeasure>(heart_rateArray.size());
+            for (int i=0; i<heart_rateArray.size(); i++) {
+                final JSONObject heartRateTuple = heart_rateArray.getJSONObject(i);
+                heartRateMeasure.timestamp = heartRateTuple.getDouble("timestamp");
+                heartRateMeasure.heartRate = heartRateTuple.getDouble("heart_rate");
+                facet.heart_rate.add(heartRateMeasure);
+            }
         }
         if (jsonObject.has("calories")) {
-            final JSONArray caloriesArray = jsonObject.getJSONArray("calories");
-            facet.calories = new double[caloriesArray.size()];
-            for (int i=0; i<caloriesArray.size(); i++)
-                facet.calories[i] = caloriesArray.getDouble(i);
+            // ignore calories for now
         }
 
         facets.add(facet);

fluxtream-connectors/src/main/java/com/fluxtream/connectors/runkeeper/RunKeeperFitnessActivityExtractorTest.java
@@ -1,21 +1,0 @@
-package com.fluxtream.connectors.runkeeper;
-
-import java.util.Date;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-import org.junit.Test;
-
-/**
- *
- * @author Candide Kemmler (candide@fluxtream.com)
- */
-public class RunKeeperFitnessActivityExtractorTest {
-
-    @Test
-    public void testDate() {
-        DateTimeFormatter timeFormatter = DateTimeFormat.forPattern("EEE, dd MMM yyyy HH:mm:ss");
-        long millis = timeFormatter.parseMillis("Thu, 3 Mar 2011 07:00:00");
-        System.out.println(new Date(millis));
-    }
-
-}

fluxtream-connectors/src/main/java/com/fluxtream/connectors/runkeeper/RunKeeperFitnessActivityFacet.java
@@ -1,6 +1,16 @@
 package com.fluxtream.connectors.runkeeper;
 
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import com.fluxtream.connectors.annotations.ObjectTypeSpec;
 import com.fluxtream.domain.AbstractFacet;
 
@@ -9,8 +19,12 @@ import com.fluxtream.domain.AbstractFacet;
  * @author Candide Kemmler (candide@fluxtream.com)
  */
 @Entity(name="Facet_RunKeeperFitnessActivity")
-@ObjectTypeSpec(name = "fitnessActivity", value = 1, extractor=RunKeeperFitnessActivityExtractor.class, prettyname = "Fitness Activity", isDateBased = true)
-public class RunKeeperFitnessActivityFacet extends AbstractFacet {
+@NamedQueries({
+      @NamedQuery(name = "runkeeper.fitnessactivity.deleteAll", query = "DELETE FROM Facet_RunKeeperFitnessActivity facet WHERE facet.guestId=?"),
+      @NamedQuery(name = "runkeeper.fitnessactivity.between", query = "SELECT facet FROM Facet_RunKeeperFitnessActivity facet WHERE facet.guestId=? AND facet.start>=? AND facet.end<=?")
+})
+@ObjectTypeSpec(name = "fitnessActivity", value = 1, extractor=RunKeeperFitnessActivityExtractor.class, prettyname = "Fitness Activity", isDateBased = false)
+    public class RunKeeperFitnessActivityFacet extends AbstractFacet {
 
     public String uri;
     public String userID;
@@ -18,16 +32,24 @@ public class RunKeeperFitnessActivityFacet extends AbstractFacet {
     public String equipment;
     public double total_distance;
     public int duration;
-    public double[] distance;
-    public double[] heart_rate;
-    public double[] calories;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "FitnessActivityDistance",
+            joinColumns = @JoinColumn(name="FitnessActivityID")
+    )
+    public List<DistanceMeasure> distance;
+    @ElementCollection
+    @CollectionTable(
+            name = "FitnessActivityHeartRate",
+            joinColumns = @JoinColumn(name="FitnessActivityID")
+    )
+    public List<HeartRateMeasure> heart_rate;
+
     public double total_climb;
     public String source;
-
-    public double [] latitudes;
-    public double [] longitudes;
-    public long [] timestamps;
-    public double [] altitudes;
+    public boolean is_live;
+    public String comments;
 
     @Override
     protected void makeFullTextIndexable() {

commit 008377d4a887908c0a91cd1c443d143de1f697e1

Modified location persistence to check for/ignore duplicates, survive exceptions in setting day metadata, and properly set guestId, timeUpdated, and api fields.  The lattermost issue was introduced in commit 2c8539c76acbbcd2576458c9ea0144d910a5d836 and fixed here.  Location data tables using versions between there and here will have all location data assigned to guestId=0.  Connectors updated with those versions need to be removed and re-added or have the ScheduledUpdate table cleared and updates called or else they will have data gaps.

fluxtream-core/src/main/java/com/fluxtream/services/impl/ApiDataServiceImpl.java
@@ -553,8 +553,43 @@ public class ApiDataServiceImpl implements ApiDataService {
     @Override
     @Transactional(readOnly = false)
     public void addGuestLocation(final long guestId, LocationFacet locationResource) {
-        updateDayMetadata(guestId, locationResource.timestampMs, locationResource.latitude, locationResource.longitude);
-        em.persist(locationResource);
+        // Create query to check for duplicate
+        Query query = em.createQuery("SELECT e FROM Facet_GoogleLatitudeLocation e WHERE e.guestId=? AND e.start=?");
+		query.setParameter(1, guestId);
+		query.setParameter(2, locationResource.timestampMs);
+		@SuppressWarnings("rawtypes")
+		List existing = query.getResultList();
+        // Only update Day metadata and persist if this location datapoint is not yet present in the DB table
+		if (existing.size()==0) {
+            // Fill in guestId, timeUpdated, and source.  These were removed in commit
+            // 2c8539c76acbbcd2576458c9ea0144d910a5d836 and location data tables using versions between
+            // there and here will have all location data assigned to guestId=0.  Connectors updated with
+            // those versions need to be removed and re-added or they will have data gaps
+            locationResource.guestId=guestId;
+            locationResource.timeUpdated=System.currentTimeMillis();
+            if(locationResource.source==LocationFacet.Source.GOOGLE_LATITUDE) {
+                locationResource.api=Connector.getConnector("google_latitude").value();
+            }
+            else {
+                locationResource.api=0;// This happens when GuestServiceImpl CheckIn function uses ip lookup
+            }
+
+            // Put updateDayMetadata in a try/catch block because we don't want to fail update or
+            // fail to persist this datapoint due to some problem in the timezone detection, etc.
+            try {
+                updateDayMetadata(guestId, locationResource.timestampMs, locationResource.latitude, locationResource.longitude);
+            } catch(Throwable e) {
+                StringBuilder sb = new StringBuilder("module=updateQueue component=apiDataServiceImpl action=addGuestLocation")
+                                    .append(" latitude=").append(locationResource.latitude)
+                                    .append(" longitude=").append(locationResource.longitude)
+                                    .append(" guestId=").append(guestId)
+                                    .append(" stackTrace=<![CDATA[").append(Utils.stackTrace(e)).append("]]>");
+                logger.warn(sb.toString());
+            }
+
+            // Persist the location
+            em.persist(locationResource);
+        }
     }
 
     @Transactional(readOnly = false)