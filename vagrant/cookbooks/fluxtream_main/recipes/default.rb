7796290920facb3cbb63df77b0e63b9e738f049b
Candide Kemmler <ckemmler@gmail.com>
2013-03-13 18:10:37 +0100

    WIP Vagrant...

---
+++ vagrant/cookbooks/fluxtream_main/recipes/default.rb
@@ -0,0 +1,12 @@
+require_recipe "apt"
+require_recipe "apache2"
+
+execute "disable-default-site" do
+  command "sudo a2dissite default"
+  notifies :reload, resources(:service => "apache2"), :delayed
+end
+
+web_app "project" do
+  template "project.conf.erb"
+  notifies :reload, resources(:service => "apache2"), :delayed
+end
