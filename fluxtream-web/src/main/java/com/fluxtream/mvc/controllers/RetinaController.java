c6fb12d69523af7460f5c4519ca0feff807738c4
Justin Loutsenhizer <nano351@gmail.com>
2013-03-26 21:01:39 -0400

    added caching of images served through the RetinaController

--- fluxtream-web/src/main/java/com/fluxtream/mvc/controllers/RetinaController.java
+++ fluxtream-web/src/main/java/com/fluxtream/mvc/controllers/RetinaController.java
@@ -12,6 +12,7 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Date;
 
 /**
  *
@@ -33,21 +34,33 @@ public class RetinaController {
             response.sendError(404);
             return;
         }
+        setCacheTime(response,31556926);
         serveRetinaImage(uri, request, response);
     }
 
     @RequestMapping({"/static/img/**", "/static/images/**", "/images/**"})
     public void serveStaticImg(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        setCacheTime(response,31556926);
         serveRetinaImage(request.getRequestURI(), request, response);
+    }
+
+    private void setCacheTime(HttpServletResponse response, long cacheAge){
+        response.setHeader("Cache-Control", "public, max-age="+ cacheAge);
+    }
+
+    private void setLastModified(HttpServletResponse response, long lastModified){
+        response.setDateHeader("Last-Modified",lastModified);
     }
 
     private void serveRetinaImage(String path, HttpServletRequest request, HttpServletResponse response) throws IOException {
         final String realPath = request.getSession().getServletContext().getRealPath("");
         path = retinaPath(path, realPath, request);
         String fullPath = realPath + path;
+        System.out.println(fullPath);
         final File file = new File(fullPath);
         FileInputStream fileinput = null;
         try {
+            setLastModified(response,file.lastModified());
             fileinput = new FileInputStream(file);
             IOUtils.copy(fileinput, response.getOutputStream());}
         catch (FileNotFoundException e) { response.sendError(404); }
