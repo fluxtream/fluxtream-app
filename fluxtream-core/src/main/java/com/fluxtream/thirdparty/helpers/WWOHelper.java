e15adff494a8eed82a3359fc26055f5598757c4c
Candide Kemmler <ckemmler@gmail.com>
2013-04-11 17:03:05 +0200

    re-enable weather service (on the condition that an API key was provided for it)

--- fluxtream-core/src/main/java/com/fluxtream/thirdparty/helpers/WWOHelper.java
+++ fluxtream-core/src/main/java/com/fluxtream/thirdparty/helpers/WWOHelper.java
@@ -2,13 +2,18 @@ package com.fluxtream.thirdparty.helpers;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import com.fluxtream.Configuration;
 import com.fluxtream.domain.metadata.DayMetadataFacet;
 import com.fluxtream.domain.metadata.WeatherInfo;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.apache.commons.httpclient.HttpException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import static com.fluxtream.utils.HttpUtils.fetch;
 
 @Component
 public class WWOHelper {
@@ -37,51 +42,56 @@ public class WWOHelper {
 
     public List<WeatherInfo> getWeatherInfo(double latitude, double longitude, String fdate) throws HttpException, IOException {
         List<WeatherInfo> weather = new ArrayList<WeatherInfo>();
-//		String wwoUrl = "http://www.worldweatheronline.com/feed/premium-weather-v2.ashx?" +
-//"key=" + env.get("wwo.key") + "&feedkey=" + env.get("wwo.feedkey") + "&format=json&q=" + latitude + "," + longitude + "&date=" + fdate;
-//		String wwoJson = fetch(wwoUrl);
-//
-//		JSONObject wwoInfo = JSONObject.fromObject(wwoJson);
-//		if (wwoInfo!=null) {
-//			JSONObject data = wwoInfo.getJSONObject("data");
-//			if (data==null) return weather;
-//			JSONArray weatherDataArray = data.getJSONArray("weather");
-//			if (weatherDataArray==null) return weather;
-//			JSONObject weatherData = weatherDataArray.getJSONObject(0);
-//			if (weatherData==null) return weather;
-//			JSONArray hourly = weatherData.getJSONArray("hourly");
-//			if (hourly!=null) {
-//				@SuppressWarnings("rawtypes")
-//				Iterator iterator = hourly.iterator();
-//				while (iterator.hasNext()) {
-//					JSONObject hourlyRecord = (JSONObject) iterator.next();
-//					WeatherInfo weatherInfo = new WeatherInfo();
-//					weatherInfo.cloudcover = Integer.valueOf(hourlyRecord.getString("cloudcover"));
-//					weatherInfo.humidity = Integer.valueOf(hourlyRecord.getString("humidity"));
-//					weatherInfo.precipMM = Float.valueOf(hourlyRecord.getString("precipMM"));
-//					weatherInfo.pressure = Integer.valueOf(hourlyRecord.getString("pressure"));
-//					weatherInfo.tempC = Integer.valueOf(hourlyRecord.getString("tempC"));
-//					weatherInfo.tempF = Integer.valueOf(hourlyRecord.getString("tempF"));
-//					weatherInfo.minuteOfDay = Integer.valueOf(hourlyRecord.getString("time"));
-//
-//					weatherInfo.visibility = Integer.valueOf(hourlyRecord.getString("visibility"));
-//					weatherInfo.weatherCode = Integer.valueOf(hourlyRecord.getString("weatherCode"));
-//					JSONArray weatherDesc = hourlyRecord.getJSONArray("weatherDesc");
-//					JSONArray weatherIconUrl = hourlyRecord.getJSONArray("weatherIconUrl");
-//					weatherInfo.weatherDesc = weatherDesc.getJSONObject(0).getString("value");
-//                    weatherInfo.weatherIconUrl = null;
-//                    weatherInfo.weatherIconUrlDay = null;
-//                    weatherInfo.weatherIconUrlNight = null;
-//                    weatherInfo.weatherIconUrl = weatherIconUrl.getJSONObject(0).getString("value");
-//					weatherInfo.winddirDegree = Integer.valueOf(hourlyRecord.getString("winddirDegree"));
-//					weatherInfo.windspeedMiles = Integer.valueOf(hourlyRecord.getString("windspeedMiles"));
-//                    weatherInfo.windspeedKmph = Integer.valueOf(hourlyRecord.getString("windspeedKmph"));
-//                    weatherInfo.winddir16Point = hourlyRecord.getString("winddir16Point");
-//
-//					weather.add(weatherInfo);
-//				}
-//			}
-//		}
+
+        // if no key is provided, just return an empty List
+        // TODO: handle this more gracefully on the frontend
+        if (env.get("wwo.key")==null)
+            return weather;
+
+		String wwoUrl = "http://www.worldweatheronline.com/feed/premium-weather-v2.ashx?" +
+"key=" + env.get("wwo.key") + "&feedkey=" + env.get("wwo.feedkey") + "&format=json&q=" + latitude + "," + longitude + "&date=" + fdate;
+		String wwoJson = fetch(wwoUrl);
+
+		JSONObject wwoInfo = JSONObject.fromObject(wwoJson);
+		if (wwoInfo!=null) {
+			JSONObject data = wwoInfo.getJSONObject("data");
+			if (data==null) return weather;
+			JSONArray weatherDataArray = data.getJSONArray("weather");
+			if (weatherDataArray==null) return weather;
+			JSONObject weatherData = weatherDataArray.getJSONObject(0);
+			if (weatherData==null) return weather;
+			JSONArray hourly = weatherData.getJSONArray("hourly");
+			if (hourly!=null) {
+				@SuppressWarnings("rawtypes") Iterator iterator = hourly.iterator();
+				while (iterator.hasNext()) {
+					JSONObject hourlyRecord = (JSONObject) iterator.next();
+					WeatherInfo weatherInfo = new WeatherInfo();
+					weatherInfo.cloudcover = Integer.valueOf(hourlyRecord.getString("cloudcover"));
+					weatherInfo.humidity = Integer.valueOf(hourlyRecord.getString("humidity"));
+					weatherInfo.precipMM = Float.valueOf(hourlyRecord.getString("precipMM"));
+					weatherInfo.pressure = Integer.valueOf(hourlyRecord.getString("pressure"));
+					weatherInfo.tempC = Integer.valueOf(hourlyRecord.getString("tempC"));
+					weatherInfo.tempF = Integer.valueOf(hourlyRecord.getString("tempF"));
+					weatherInfo.minuteOfDay = Integer.valueOf(hourlyRecord.getString("time"));
+
+					weatherInfo.visibility = Integer.valueOf(hourlyRecord.getString("visibility"));
+					weatherInfo.weatherCode = Integer.valueOf(hourlyRecord.getString("weatherCode"));
+					JSONArray weatherDesc = hourlyRecord.getJSONArray("weatherDesc");
+					JSONArray weatherIconUrl = hourlyRecord.getJSONArray("weatherIconUrl");
+					weatherInfo.weatherDesc = weatherDesc.getJSONObject(0).getString("value");
+                    weatherInfo.weatherIconUrl = null;
+                    weatherInfo.weatherIconUrlDay = null;
+                    weatherInfo.weatherIconUrlNight = null;
+                    weatherInfo.weatherIconUrl = weatherIconUrl.getJSONObject(0).getString("value");
+					weatherInfo.winddirDegree = Integer.valueOf(hourlyRecord.getString("winddirDegree"));
+					weatherInfo.windspeedMiles = Integer.valueOf(hourlyRecord.getString("windspeedMiles"));
+                    weatherInfo.windspeedKmph = Integer.valueOf(hourlyRecord.getString("windspeedKmph"));
+                    weatherInfo.winddir16Point = hourlyRecord.getString("winddir16Point");
+
+					weather.add(weatherInfo);
+				}
+			}
+		}
 		return weather;
 	}
 }
