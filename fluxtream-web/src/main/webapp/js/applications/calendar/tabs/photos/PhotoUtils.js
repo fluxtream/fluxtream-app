8281b44906b9bdb448831023d44d4d8242d24f7b
Justin Loutsenhizer <nano351@gmail.com>
2013-05-13 12:28:53 -0400

    The photo carousel now properly rotates images based on their orientation

--- fluxtream-web/src/main/webapp/js/applications/calendar/tabs/photos/PhotoUtils.js
+++ fluxtream-web/src/main/webapp/js/applications/calendar/tabs/photos/PhotoUtils.js
@@ -6,6 +6,16 @@ define([],function(){
         carouselTemplate = template;
     });
 
+    var orientationStyles = ["none",//1
+                             "scale(-1,1)",//2
+                             "rotate(180deg)",//3
+                             "rotate(180deg) scale(-1,1)",//4
+                             "rotate(90deg) scale(1,-1)",//5
+                             "rotate(90deg)",//6
+                             "rotate(270deg) scale(1,-1)",//7
+                             "rotate(270deg)"//8
+    ];
+
     function getCarouselHTML(digest,connectorNames){
         if (connectorNames == null){//if the parameter is discluded, show all images
             connectorNames = [];
@@ -24,15 +34,16 @@ define([],function(){
         for (var i = 0; i < connectorNames.length; i++){
             for (var j = 0; digest.cachedData[connectorNames[i]] != null && digest.cachedData[connectorNames[i]].hasImages && j < digest.cachedData[connectorNames[i]].length; j++){
                 for (var k = 0; digest.cachedData[connectorNames[i]][j].hasImage && k <= data.length; k++){
-                    if (k == data.length){
-                        data[k] = digest.cachedData[connectorNames[i]][j];
-                        break;
-                    }
-                    if (data[k].start > digest.cachedData[connectorNames[i]][j].start || digest.cachedData[connectorNames[i]][j].start == null){
+                    if (k == data.length || data[k].start > digest.cachedData[connectorNames[i]][j].start || digest.cachedData[connectorNames[i]][j].start == null){
                         data.splice(k,0,digest.cachedData[connectorNames[i]][j]);
+                        if (data[k].orientation != null){
+                            data[k].transformations = orientationStyles[data[k].orientation-1];
+                        }
+                        if (data[k].transformations == null || data[k].transformations == "")
+                            data[k].transformations = "none";
+
                         break;
                     }
-
                 }
             }
         }
