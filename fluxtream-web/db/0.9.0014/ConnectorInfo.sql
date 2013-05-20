f2c5083c155459d8b411903f261e4808ccc913bd
Candide Kemmler <ckemmler@gmail.com>
2013-05-20 15:52:47 +0200

    typo

--- fluxtream-web/db/0.9.0014/ConnectorInfo.sql
+++ fluxtream-web/db/0.9.0014/ConnectorInfo.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `Connector` ADD COLUMN `supportsRenewToken` char(1) NOT NULL DEFAULT 'N';
+ALTER TABLE `Connector` ADD COLUMN `supportsRenewTokens` char(1) NOT NULL DEFAULT 'N';
 DELETE FROM `Connector`;
