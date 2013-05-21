685f2fd98c7159fb68595dea009f78ffc6dbbdc5
Candide Kemmler <ckemmler@gmail.com>
2013-05-21 20:14:38 +0200

    WIP: added unit tests

---
+++ fluxtream-core/src/test/java/com/fluxtream/utils/TimeSegmentTest.java
@@ -0,0 +1,23 @@
+package com.fluxtream.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * User: candide
+ * Date: 21/05/13
+ * Time: 11:29
+ */
+public class TimeSegmentTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testQuerypoint() {
+
+    }
+
+}
