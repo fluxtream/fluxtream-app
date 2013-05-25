f199d4eedc549f637a4d4aacc755d84a24362652
Candide Kemmler <ckemmler@gmail.com>
2013-05-25 18:25:08 +0200

    delete VisitedCity entries that were derived from a Location-type facet when we remove its associated connector

--- fluxtream-core/src/main/java/com/fluxtream/connectors/dao/JPAFacetDao.java
+++ fluxtream-core/src/main/java/com/fluxtream/connectors/dao/JPAFacetDao.java
@@ -17,6 +17,7 @@ import com.fluxtream.connectors.location.LocationFacet;
 import com.fluxtream.domain.AbstractFacet;
 import com.fluxtream.domain.ApiKey;
 import com.fluxtream.domain.TagFilter;
+import com.fluxtream.domain.metadata.VisitedCity;
 import com.fluxtream.services.ConnectorUpdateService;
 import com.fluxtream.services.GuestService;
 import com.fluxtream.utils.JPAUtils;
@@ -299,10 +300,20 @@ public class JPAFacetDao implements FacetDao {
                 bulkDeleteFacets(apiKey, facetClass);
             }
             final LocationFacet.Source locationFacetSource = getLocationFacetSource(facetClass);
-            if (locationFacetSource != LocationFacet.Source.NONE)
+            if (locationFacetSource != LocationFacet.Source.NONE) {
                 deleteLocationData(apiKey);
+                deleteVisitedCitiesData(apiKey);
+            }
         }
 	}
+
+    private void deleteVisitedCitiesData(final ApiKey apiKey) {
+        final String facetName = getEntityName(VisitedCity.class);
+        String stmtString = "DELETE FROM " + facetName + " facet WHERE facet.apiKeyId=?";
+        final Query query = em.createQuery(stmtString);
+        query.setParameter(1, apiKey.getId());
+        query.executeUpdate();
+    }
 
     private void deleteLocationData(final ApiKey apiKey) {
         final String facetName = getEntityName(LocationFacet.class);
