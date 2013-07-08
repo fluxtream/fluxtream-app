abd33e3ec8ad517e8e750ac444e5ae044901e1af
Justin Loutsenhizer <nano351@gmail.com>
2013-07-08 13:23:29 -0400

    Added requirejs optimization via nodejs to the maven build

---
+++ fluxtream-web/src/main/app.build.js
@@ -0,0 +1,10 @@
+({
+    appDir: "${basedir}/src/main/webapp/",
+    baseUrl: "js",
+    dir: "${basedir}/src/main/webapp/optimized/",
+    modules: [
+        {
+            name: "main"
+        }
+    ]
+})
