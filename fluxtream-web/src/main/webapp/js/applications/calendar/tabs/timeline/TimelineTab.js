887209d662514281895960a7d86862777b1d75e6
Justin Loutsenhizer <nano351@gmail.com>
2013-07-24 12:42:18 -0400

    corrected timeline tab to not request digest on timespan label change

--- fluxtream-web/src/main/webapp/js/applications/calendar/tabs/timeline/TimelineTab.js
+++ fluxtream-web/src/main/webapp/js/applications/calendar/tabs/timeline/TimelineTab.js
@@ -63,7 +63,7 @@ define(["core/Tab", "core/FlxState", "core/grapher/Grapher",
         if (state.tabState != prevTabState
                 && stateEarly.tabState != prevTabState
                 && stateLate.tabState != prevTabState) {
-            Calendar.changeTabState(state, false);
+            Calendar.changeTabState(state, true);
             Calendar.timespanInited = false; // Need to be ready to refresh the date display if user hits back button
             return state;
         }
