b8fceac98ac8dafe5355c763563805ccd705fff6
Justin Loutsenhizer <nano351@gmail.com>
2013-08-01 15:06:46 -0400

    fixed wrong cities being shown for facets in day view

--- fluxtream-web/src/main/webapp/js/App.js
+++ fluxtream-web/src/main/webapp/js/App.js
@@ -547,8 +547,9 @@ define(
         };
 
 
-        //TODO: this needs to be done better
         App.getFacetCity = function(facet, metadata){
+            if (metadata.timeUnit === "DAY")
+                return metadata.mainCity;
             var cities = metadata.cities;
             var time = (facet.start + (facet.end != null ? facet.end : facet.start)) / 2
             for (var i=0; i<cities.length; i++) {
@@ -556,8 +557,6 @@ define(
                 if ((city.dayStart<=time && time<city.dayEnd) || (city.dayStart<=facet.start && facet.start<city.dayEnd))
                     return city;
             }
-            if ((metadata.mainCity.dayStart<=time && time<metadata.mainCity.dayEnd) || (metadata.mainCity.dayStart<=facet.start && facet.start<metadata.mainCity.dayEnd))
-                return metadata.mainCity;
             console.log("WARNING: facet isn't within metadata time boundaries: " + new Date(facet.start))
             console.log(facet);
             return null;
