ee58d67486ab535fa70379d4dad6582ae178b7db
arwright <anne.r.wright@gmail.com>
2013-10-07 17:13:01 -0400

    Incomplete attempt at adding STATUS_PERMANENT_FAILURE reporting using UpdateFailedException.  Added detection for such failures to refresh/swap token handlers for GoogleOAuth, Moves, and BodyMedia, as well as user notifications.  Also modified UpdateWorker to only reschedule regular STATUS_FAILURE and mark the failure as temporary in the case that the connector was not already marked permanently failed.

---
+++ fluxtream-core/src/main/java/com/fluxtream/connectors/updaters/UpdateFailedException.java
@@ -0,0 +1,11 @@
+package com.fluxtream.connectors.updaters;
+
+/**
+ * <p>
+ * <code>UpdateFailedException</code> does something...
+ * </p>
+ *
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public class UpdateFailedException {
+}
