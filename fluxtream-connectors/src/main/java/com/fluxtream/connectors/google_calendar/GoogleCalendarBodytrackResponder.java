51fa295abbab1cf524a10308573e395126ced234
Candide Kemmler <candide@fluxtream.org>
2013-10-21 15:49:28 +0200

    hide events from hidden calendar as per user preferences

--- fluxtream-connectors/src/main/java/com/fluxtream/connectors/google_calendar/GoogleCalendarBodytrackResponder.java
+++ fluxtream-connectors/src/main/java/com/fluxtream/connectors/google_calendar/GoogleCalendarBodytrackResponder.java
@@ -9,11 +9,14 @@ import com.fluxtream.TimeUnit;
 import com.fluxtream.connectors.Connector;
 import com.fluxtream.connectors.ObjectType;
 import com.fluxtream.connectors.bodytrackResponders.AbstractBodytrackResponder;
+import com.fluxtream.connectors.updaters.UpdateFailedException;
 import com.fluxtream.connectors.vos.AbstractFacetVO;
 import com.fluxtream.domain.AbstractFacet;
 import com.fluxtream.domain.ApiKey;
 import com.fluxtream.domain.GuestSettings;
 import com.fluxtream.mvc.models.TimespanModel;
+import com.fluxtream.services.SettingsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -24,15 +27,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class GoogleCalendarBodytrackResponder extends AbstractBodytrackResponder {
 
+    @Autowired
+    SettingsService settingsService;
+
     @Override
     public List<TimespanModel> getTimespans(final long startMillis, final long endMillis, final ApiKey apiKey, final String channelName) {
         List<TimespanModel> items = new ArrayList<TimespanModel>();
+        GoogleCalendarConnectorSettings connectorSettings = null;
+        try { connectorSettings = (GoogleCalendarConnectorSettings)settingsService.getConnectorSettings(apiKey.getId(), false);
+        } catch (UpdateFailedException e) {}
         final TimeInterval timeInterval = new SimpleTimeInterval(startMillis, endMillis, TimeUnit.ARBITRARY, TimeZone.getTimeZone("UTC"));
         String objectTypeName = apiKey.getConnector().getName() + "-event";
         List<AbstractFacet> facets = getFacetsInTimespan(timeInterval,apiKey,null);
         for (AbstractFacet facet : facets){
             GoogleCalendarEventFacet event = (GoogleCalendarEventFacet) facet;
-            items.add(new TimespanModel(event.start, event.end, ((GoogleCalendarEventFacet)facet).calendarId, objectTypeName));
+            if (connectorSettings!=null) {
+                final CalendarConfig calendarConfig = connectorSettings.getCalendar(event.calendarId);
+                if (calendarConfig!=null&&!calendarConfig.hidden)
+                    items.add(new TimespanModel(event.start, event.end, ((GoogleCalendarEventFacet)facet).calendarId, objectTypeName));
+            } else
+                items.add(new TimespanModel(event.start, event.end, ((GoogleCalendarEventFacet)facet).calendarId, objectTypeName));
         }
         return items;
     }
