2ffc29f3a5ab27521dd8d68349c295ecfbe861d1
Candide Kemmler <candide@fluxtream.org>
2013-12-10 15:11:38 +0100

    provided access point for EN resources

--- fluxtream-connectors/src/main/java/com/fluxtream/connectors/evernote/EvernoteController.java
+++ fluxtream-connectors/src/main/java/com/fluxtream/connectors/evernote/EvernoteController.java
@@ -1,14 +1,19 @@
 package com.fluxtream.connectors.evernote;
 
 import java.io.IOException;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import com.fluxtream.Configuration;
 import com.fluxtream.auth.AuthHelper;
 import com.fluxtream.connectors.Connector;
 import com.fluxtream.domain.ApiKey;
 import com.fluxtream.domain.Guest;
 import com.fluxtream.services.GuestService;
+import com.fluxtream.services.JPADaoService;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.EvernoteApi;
 import org.scribe.model.Token;
@@ -16,6 +21,7 @@ import org.scribe.model.Verifier;
 import org.scribe.oauth.OAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -35,6 +41,11 @@ public class EvernoteController {
     @Autowired
     GuestService guestService;
 
+    @Autowired
+    JPADaoService jpaDaoService;
+
+    @PersistenceContext
+    EntityManager em;
 
     @RequestMapping(value = "/token")
     public String getEvernoteToken(HttpServletRequest request) throws IOException, ServletException {
@@ -85,6 +96,16 @@ public class EvernoteController {
 
     String getConsumerSecret() {
         return env.get("evernoteConsumerSecret");
+    }
+
+    @RequestMapping(value="/res/{guid}")
+    public void getResource(@PathVariable("guid") String guid,
+                            HttpServletResponse response) throws IOException {
+        final Query nativeQuery = em.createNativeQuery(String.format("SELECT mime, dataBody FROM Facet_EvernoteResource WHERE guid='%s'", guid));
+        final Object[] singleResult = (Object[])nativeQuery.getSingleResult();
+        response.setContentType((String)singleResult[0]);
+        byte[] resourceData = (byte[])singleResult[1];
+        response.getOutputStream().write(resourceData, 0, resourceData.length);
     }
 
 
