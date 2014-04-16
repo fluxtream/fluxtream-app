f41e2b2e8141bc9633e5f90e0ddb3214540f6b64
Candide Kemmler <candide@fluxtream.org>
2014-04-16 22:57:17 +0200

    added some REST API documentation

--- fluxtream-core/src/main/java/org/fluxtream/core/api/MetadataController.java
+++ fluxtream-core/src/main/java/org/fluxtream/core/api/MetadataController.java
@@ -1,20 +1,9 @@
 package org.fluxtream.core.api;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeSet;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.CacheControl;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import com.google.gson.Gson;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 import org.fluxtream.core.Configuration;
 import org.fluxtream.core.aspects.FlxLogger;
 import org.fluxtream.core.auth.AuthHelper;
@@ -25,11 +14,19 @@ import org.fluxtream.core.mvc.models.VisitedCityModel;
 import org.fluxtream.core.services.GuestService;
 import org.fluxtream.core.services.MetadataService;
 import org.fluxtream.core.utils.TimeUtils;
-import com.google.gson.Gson;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
 
 /**
  * User: candide
@@ -37,6 +34,7 @@ import org.springframework.stereotype.Component;
  * Time: 12:07
  */
 @Path("/metadata")
+@Api(value = "/metadata", description = "Location/Timezone query and disambiguation endpoints.")
 @Component("RESTMetadataController")
 @Scope("request")
 public class MetadataController {
@@ -56,10 +54,12 @@ public class MetadataController {
 
     @POST
     @Path(value="/mainCity/date/{date}")
+    @ApiOperation(value = "Set the main city for a given day using lat/lon coordinates.", response = StatusModel.class,
+            notes="(we figure out the actual city from the coordinates)")
     @Produces({ MediaType.APPLICATION_JSON } )
-    public StatusModel setDayMainCity(@FormParam("latitude") float latitude,
-                                      @FormParam("longitude") float longitude,
-                                      @PathParam("date") String date) {
+    public StatusModel setDayMainCity(@ApiParam(value="Latitude", required=true) @FormParam("latitude") float latitude,
+                                      @ApiParam(value="Longitude", required=true) @FormParam("longitude") float longitude,
+                                      @ApiParam(value="Date (YYYY-MM-DD)", required=true) @PathParam("date") String date) {
         final long guestId = AuthHelper.getGuestId();
         StringBuilder sb = new StringBuilder("module=API component=calendarController action=setDayMainCity")
                 .append(" guestId=").append(guestId);
@@ -70,8 +70,9 @@ public class MetadataController {
 
     @DELETE
     @Path(value="/mainCity/date/{date}")
+    @ApiOperation(value = "Remove cities that have been manually entered by the end-user.", response = StatusModel.class)
     @Produces({ MediaType.APPLICATION_JSON } )
-    public StatusModel resetDayMainCity(@PathParam("date") String date) {
+    public StatusModel resetDayMainCity(@ApiParam(value="Date (YYYY-MM-DD)", required=true) @PathParam("date") String date) {
         final long guestId = AuthHelper.getGuestId();
         StringBuilder sb = new StringBuilder("module=API component=calendarController action=resetDayMainCity")
                 .append(" guestId=").append(guestId);
@@ -82,9 +83,10 @@ public class MetadataController {
 
     @POST
     @Path(value="/mainCity/{visitedCityId}/date/{date}")
+    @ApiOperation(value = "Set a given city and associated timezone to be the reference for a given day.", response = StatusModel.class)
     @Produces({ MediaType.APPLICATION_JSON } )
-    public StatusModel setDayMainCity(@PathParam("visitedCityId") long visitedCityId,
-                                      @PathParam("date") String date) {
+    public StatusModel setDayMainCity(@ApiParam(value="ID of the city (as in /metadata/cities)", required=true) @PathParam("visitedCityId") long visitedCityId,
+                                      @ApiParam(value="Date (YYYY-MM-DD)", required=true) @PathParam("date") String date) {
         final long guestId = AuthHelper.getGuestId();
         StringBuilder sb = new StringBuilder("module=API component=calendarController action=setDayMainCity")
                 .append(" guestId=").append(guestId);
@@ -95,8 +97,9 @@ public class MetadataController {
 
     @GET
     @Path(value="/foursquare/venue/{venueId}")
+    @ApiOperation(value = "Retrieve the Foursquare info for a given venue ID (results are cached)", response = FoursquareVenue.class)
     @Produces({ MediaType.APPLICATION_JSON } )
-    public Response getFoursquareVenue(@PathParam("venueId") String venueId) {
+    public Response getFoursquareVenue(@ApiParam(value="Foursquare venue ID", required=true) @PathParam("venueId") String venueId) {
         // this doesn't seem to have any effect, i.e. the cache-control header is alwasy set to no-cache
         // needs invistigating...
         CacheControl cc = new CacheControl();
@@ -111,8 +114,10 @@ public class MetadataController {
 
     @GET
     @Path(value = "/checkIn/{ipAddress}")
+    @ApiOperation(value = "Use ip2location lookup to guess the user's location based on his IP address", response = StatusModel.class,
+        notes="The resulting location will interpreted as a place the user was at at that moment")
     @Produces({MediaType.APPLICATION_JSON})
-    public StatusModel checkIn(@PathParam("ipAddress") String ipAddress){
+    public StatusModel checkIn(@ApiParam(value="The end-users terminal IP address", required=true) @PathParam("ipAddress") String ipAddress){
         final long guestId = AuthHelper.getGuestId();
         try {
             guestService.checkIn(guestId, ipAddress);
@@ -125,8 +130,11 @@ public class MetadataController {
 
     @GET
     @Path(value = "/cities")
+    @ApiOperation(value = "The list of cities visited by the user during a given time interval", responseContainer = "Array",
+            response = VisitedCityModel.class)
     @Produces({MediaType.APPLICATION_JSON})
-    public String getCitiesForRange(@QueryParam("start") long start, @QueryParam("end") long end){
+    public String getCitiesForRange(@ApiParam(value="Start of given time interval", required=true) @QueryParam("start") long start,
+                                    @ApiParam(value="End of given time interval", required=true) @QueryParam("end") long end){
         TreeSet<String> dates = new TreeSet<String>();
         DateTime startDate = new DateTime(start);
         DateTime endDate = new DateTime(end);
