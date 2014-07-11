774808cd84a4503a9d02a7e4eff11008f668bc0b
Candide Kemmler <candide@fluxtream.org>
2014-07-11 13:08:43 +0200

    added an appId to the Guest entity as well an and API registration method to keep track of guests that have been created by partner apps

--- fluxtream-core/src/main/java/org/fluxtream/core/domain/Guest.java
+++ fluxtream-core/src/main/java/org/fluxtream/core/domain/Guest.java
@@ -1,13 +1,14 @@
 package org.fluxtream.core.domain;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.annotations.Index;
+
 import javax.persistence.Entity;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import org.apache.commons.lang.StringUtils;
-import org.hibernate.annotations.Index;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name="Guest")
 @NamedQueries ( {
@@ -26,9 +27,11 @@ public class Guest extends AbstractEntity implements Serializable {
 
 	public static final String ROLE_USER = "ROLE_USER";
 	public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public String appId;
 
     public enum RegistrationMethod {
-        REGISTRATION_METHOD_FORM, REGISTRATION_METHOD_FACEBOOK, REGISTRATION_METHOD_FACEBOOK_WITH_PASSWORD;
+        REGISTRATION_METHOD_FORM, REGISTRATION_METHOD_FACEBOOK, REGISTRATION_METHOD_FACEBOOK_WITH_PASSWORD,
+        REGISTRATION_METHOD_API;
     }
 
 	@Index(name="username_index")
