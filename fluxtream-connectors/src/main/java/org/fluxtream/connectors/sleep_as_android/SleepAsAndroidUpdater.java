66d9d9fe7c3c1316d35d7f8333a39ac16c3799d7
jloutsenhizer <nano351@gmail.com>
2014-10-08 13:49:18 -0400

    Sleep As Android connector now creates SleepFacets with all the data returned from the SleepCloud API

--- fluxtream-connectors/src/main/java/org/fluxtream/connectors/sleep_as_android/SleepAsAndroidUpdater.java
+++ fluxtream-connectors/src/main/java/org/fluxtream/connectors/sleep_as_android/SleepAsAndroidUpdater.java
@@ -6,6 +6,9 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.gdata.util.common.base.Pair;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ResponseHandler;
@@ -17,10 +20,12 @@ import org.fluxtream.core.connectors.updaters.UpdateFailedException;
 import org.fluxtream.core.connectors.updaters.UpdateInfo;
 import org.fluxtream.core.domain.ApiKey;
 import org.fluxtream.core.domain.Notification;
+import org.fluxtream.core.services.ApiDataService;
 import org.fluxtream.core.utils.Utils;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 @Updater(prettyName = "Sleep_As_Android", value = 351, objectTypes={SleepFacet.class})
@@ -42,14 +47,83 @@ public class SleepAsAndroidUpdater extends AbstractUpdater {
         HttpResponse response = env.getHttpClient().execute(get);
         String content;
         int statusCode = response.getStatusLine().getStatusCode();
-        if (statusCode == HttpStatus.SC_OK) {
-            ResponseHandler<String> responseHandler = new BasicResponseHandler();
-            content = responseHandler.handleResponse(response);
-            System.out.println(content);
-        } else {
-            System.err.println("Got status code " + statusCode);
+        if (statusCode != HttpStatus.SC_OK) {
+            throw new UpdateFailedException("Got status code " + statusCode);
         }
-        System.out.println("We should pull some data now!");
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+        content = responseHandler.handleResponse(response);
+        try{
+            createOrUpdateFacets(updateInfo,content);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            throw new UpdateFailedException(e.getMessage());
+        }
+
+    }
+
+    private long createOrUpdateFacets(UpdateInfo updateInfo, String json) throws Exception {
+        JSONObject topLevelObject = JSONObject.fromObject(json);
+        JSONArray sleepsArray = topLevelObject.getJSONArray("sleeps");
+        long oldestFacetTime = Long.MIN_VALUE;
+        for (int i = 0; i < sleepsArray.size(); i++){
+            SleepFacet newFacet = createOrUpdateFacet(updateInfo, sleepsArray.getJSONObject(i));
+            oldestFacetTime = Math.max(oldestFacetTime, newFacet.end);
+        }
+        return oldestFacetTime;
+    }
+
+
+    private SleepFacet createOrUpdateFacet(final UpdateInfo updateInfo, final JSONObject sleepObject) throws Exception {
+        return apiDataService.createOrReadModifyWrite(SleepFacet.class,new ApiDataService.FacetQuery(
+                "e.apiKeyId = ? AND e.start = ? AND e.end = ?",
+                updateInfo.apiKey.getId(), sleepObject.getLong("fromTime"), sleepObject.getLong("toTime")),
+                new ApiDataService.FacetModifier<SleepFacet>() {
+                    @Override
+                    public SleepFacet createOrModify(SleepFacet facet, Long apiKeyId) {
+                        if (facet == null){
+                            facet = new SleepFacet(updateInfo.apiKey.getId());
+                            facet.api = updateInfo.apiKey.getConnector().value();
+                            facet.start = sleepObject.getLong("fromTime");
+                            facet.end = sleepObject.getLong("toTime");
+                        }
+                        facet.cycles = sleepObject.getInt("cycles");
+                        facet.ratioDeepSleep = sleepObject.getDouble("deepSleep");
+                        facet.rating = sleepObject.getDouble("rating");
+                        facet.noiseLevel = sleepObject.getDouble("noiseLevel");
+                        if (sleepObject.has("snroingSeconds")) {
+                            facet.snoringSeconds = sleepObject.getInt("snoringSeconds");
+                        }
+                        else {
+                            facet.snoringSeconds = null;
+
+                        }
+
+                        facet.sleepTags = new ArrayList<String>();
+                        if (sleepObject.has("tags")) {
+                            JSONArray tags = sleepObject.getJSONArray("tags");
+                            for (int i = 0; i < tags.size(); i++) {
+                                facet.sleepTags.add(tags.getString(i));
+                            }
+                        }
+
+                        facet.actiGraph = new ArrayList<Double>();
+                        JSONArray actiGraph = sleepObject.getJSONArray("actigraph");
+                        for (int i = 0; i < actiGraph.size(); i++) {
+                            facet.actiGraph.add(actiGraph.getDouble(i));
+                        }
+
+                        facet.eventLabels = new ArrayList<Pair<String,Long>>();
+                        JSONArray labels = sleepObject.getJSONArray("labels");
+                        for (int i = 0; i < labels.size(); i++) {
+                            JSONObject label = labels.getJSONObject(i);
+                            facet.eventLabels.add(new Pair<String,Long>(label.getString("label"), label.getLong("timestamp")));
+                        }
+                        return facet;
+                    }
+
+                },updateInfo.apiKey.getId());
+
 
     }
 
--- fluxtream-connectors/src/main/java/org/fluxtream/connectors/sleep_as_android/SleepFacet.java
+++ fluxtream-connectors/src/main/java/org/fluxtream/connectors/sleep_as_android/SleepFacet.java
@@ -1,14 +1,53 @@
 package org.fluxtream.connectors.sleep_as_android;
 
 
+import com.google.gdata.util.common.base.Pair;
 import org.fluxtream.core.connectors.annotations.ObjectTypeSpec;
 import org.fluxtream.core.domain.AbstractFacet;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.Lob;
+import java.util.ArrayList;
+import java.util.List;
+
+// note: in this class we used ArrayList<T> for lists instead of the generic List<T> structure since ArrayList<T>
+// implements serializable interface but List<T> does not!
 
 @Entity(name="Facet_SleepAsAndroidSleep")
-@ObjectTypeSpec(name = "sleep", value = 1, prettyname = "Sleep Logs", extractor=SleepFacetExtractor.class)
+@ObjectTypeSpec(name = "sleep", value = 1, prettyname = "Sleep Logs")
 public class SleepFacet extends AbstractFacet {
+
+    public SleepFacet(){super();}
+    public SleepFacet(long apiKeyId){super(apiKeyId);}
+
+    //The number of sleep cycles went through
+    public int cycles;
+    //0.0 - 1.0 where 0 means no deep sleep and 1 means all deep sleep
+    public double ratioDeepSleep;
+
+    //0.0-5.0 rating out of 5 of the sleep quality
+    public double rating;
+
+    //how many seconds was there snroing (this can be absent)
+    public Integer snoringSeconds;
+
+    //what the noise level was like
+    public double noiseLevel;
+
+    //the actigaph is evenly sampled over the duration of the sleep. each value represents activity level (no upper bound)
+    @Lob
+    public ArrayList<Double> actiGraph;
+
+    //Tags associated with this sleep
+    @Lob
+    public ArrayList<String> sleepTags;
+
+    //Labels different events that occur throughout the night
+    @Lob
+    public ArrayList<Pair<String,Long>> eventLabels;
+
+
     @Override
     protected void makeFullTextIndexable() {
 
