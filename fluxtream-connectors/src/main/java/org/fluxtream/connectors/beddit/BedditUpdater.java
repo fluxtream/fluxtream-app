538284ea9ec2905d6913fc890bb0a72a31696c8c
jloutsenhizer <nano351@gmail.com>
2014-11-24 00:06:34 -0500

    WIP: full Beddit data import and bodytrack responder

--- fluxtream-connectors/src/main/java/org/fluxtream/connectors/beddit/BedditUpdater.java
+++ fluxtream-connectors/src/main/java/org/fluxtream/connectors/beddit/BedditUpdater.java
@@ -6,6 +6,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.gdata.util.common.base.Pair;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.apache.commons.lang.exception.ExceptionUtils;
@@ -19,17 +20,26 @@ import org.fluxtream.core.connectors.updaters.AbstractUpdater;
 import org.fluxtream.core.connectors.updaters.UpdateFailedException;
 import org.fluxtream.core.connectors.updaters.UpdateInfo;
 import org.fluxtream.core.domain.ApiKey;
+import org.fluxtream.core.domain.ChannelMapping;
 import org.fluxtream.core.domain.Notification;
 import org.fluxtream.core.services.ApiDataService;
+import org.fluxtream.core.services.impl.BodyTrackHelper;
 import org.fluxtream.core.utils.Utils;
 import org.joda.time.DateTimeZone;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Component
-@Updater(prettyName = "Beddit", value = 352, objectTypes={SleepFacet.class})
+@Updater(prettyName = "Beddit", value = 352, objectTypes={SleepFacet.class}, bodytrackResponder=BedditBodytrackResponder.class)
 public class BedditUpdater extends AbstractUpdater {
+
+    @Autowired
+    BodyTrackHelper bodyTrackHelper;
 
     //note: all timestamps but the updated timestamp are in local time, use the provided timezone ID to deal with this
 
@@ -42,6 +52,72 @@ public class BedditUpdater extends AbstractUpdater {
 
     @Override
     protected void updateConnectorData(UpdateInfo updateInfo) throws Exception {
+        List<ChannelMapping> mappings = bodyTrackHelper.getChannelMappings(updateInfo.apiKey);
+        if (mappings.size() == 0){
+            ChannelMapping mapping = new ChannelMapping();
+            mapping.deviceName = "beddit";
+            mapping.channelName = "sleep_stages";
+            mapping.timeType = ChannelMapping.TimeType.gmt;
+            mapping.channelType = ChannelMapping.ChannelType.timespan;
+            mapping.guestId = updateInfo.getGuestId();
+            mapping.apiKeyId = updateInfo.apiKey.getId();
+            bodyTrackHelper.persistChannelMapping(mapping);
+
+            BodyTrackHelper.ChannelStyle channelStyle = new BodyTrackHelper.ChannelStyle();
+            channelStyle.timespanStyles = new BodyTrackHelper.MainTimespanStyle();
+            channelStyle.timespanStyles.defaultStyle = new BodyTrackHelper.TimespanStyle();
+            channelStyle.timespanStyles.defaultStyle.fillColor = "#33b5e5";
+            channelStyle.timespanStyles.defaultStyle.borderColor = "#33b5e5";
+            channelStyle.timespanStyles.defaultStyle.borderWidth = 0;
+            channelStyle.timespanStyles.defaultStyle.top = 1.00;
+            channelStyle.timespanStyles.defaultStyle.bottom = 0.67;
+            channelStyle.timespanStyles.values = new HashMap();
+
+            BodyTrackHelper.TimespanStyle stylePart = new BodyTrackHelper.TimespanStyle();
+            stylePart.top = 0.67;
+            stylePart.bottom = 1.00;
+            stylePart.fillColor = "#33b5e5";
+            stylePart.borderColor = "#33b5e5";
+            channelStyle.timespanStyles.values.put("away",stylePart);
+
+            stylePart = new BodyTrackHelper.TimespanStyle();
+            stylePart.top = 0.33;
+            stylePart.bottom = 0.67;
+            stylePart.fillColor = "#0099cc";
+            stylePart.borderColor = "#0099cc";
+            channelStyle.timespanStyles.values.put("awake",stylePart);
+
+            stylePart = new BodyTrackHelper.TimespanStyle();
+            stylePart.top = 0.00;
+            stylePart.bottom = 0.33;
+            stylePart.fillColor = "#ff8800";
+            stylePart.borderColor = "#ff8800";
+            channelStyle.timespanStyles.values.put("asleep",stylePart);
+
+            bodyTrackHelper.setBuiltinDefaultStyle(updateInfo.getGuestId(),"beddit","sleep_stages",channelStyle);
+
+            mapping = new ChannelMapping();
+            mapping.deviceName = "beddit";
+            mapping.channelName = "snoring_episodes";
+            mapping.timeType = ChannelMapping.TimeType.gmt;
+            mapping.channelType = ChannelMapping.ChannelType.timespan;
+            mapping.guestId = updateInfo.getGuestId();
+            mapping.apiKeyId = updateInfo.apiKey.getId();
+            bodyTrackHelper.persistChannelMapping(mapping);
+
+            channelStyle = new BodyTrackHelper.ChannelStyle();
+            channelStyle.timespanStyles = new BodyTrackHelper.MainTimespanStyle();
+            channelStyle.timespanStyles.defaultStyle = new BodyTrackHelper.TimespanStyle();
+            channelStyle.timespanStyles.defaultStyle.fillColor = "#33b5e5";
+            channelStyle.timespanStyles.defaultStyle.borderColor = "#33b5e5";
+            channelStyle.timespanStyles.defaultStyle.borderWidth = 0;
+            channelStyle.timespanStyles.defaultStyle.top = 1.00;
+            channelStyle.timespanStyles.defaultStyle.bottom = 0.0;
+            channelStyle.timespanStyles.values = new HashMap();
+
+            bodyTrackHelper.setBuiltinDefaultStyle(updateInfo.getGuestId(),"beddit","snoring_episodes",channelStyle);
+        }
+
         long userId = Long.parseLong(guestService.getApiKeyAttribute(updateInfo.apiKey, "userid"));
         String accessToken = guestService.getApiKeyAttribute(updateInfo.apiKey, "access_token");
 
@@ -114,8 +190,8 @@ public class BedditUpdater extends AbstractUpdater {
                         facet.sleepTimeTarget = propertiesObject.getDouble("sleep_time_target");
                         facet.snoringAmount = propertiesObject.getDouble("total_snoring_episode_duration");
                         facet.restingHeartRate = propertiesObject.getDouble("resting_heart_rate");
-                        facet.respirationRate = propertiesObject.getDouble("average_respiration_rate");
-                        facet.timeToFallAsleep = propertiesObject.getDouble("sleep_latency");
+                        facet.respirationRate = propertiesObject.has("average_respiration_rate") ? propertiesObject.getDouble("average_respiration_rate") : 0.0;
+                        facet.timeToFallAsleep = propertiesObject.has("sleep_latency") ? propertiesObject.getDouble("sleep_latency") : 0.0;
                         facet.awayCount = (int) propertiesObject.getDouble("away_episode_count");
                         facet.totalAwayTime = propertiesObject.getDouble("stage_duration_A");
                         facet.totalSleepTime = propertiesObject.getDouble("stage_duration_S");
@@ -128,6 +204,46 @@ public class BedditUpdater extends AbstractUpdater {
                         facet.scoreFallAsleepTime = propertiesObject.getDouble("score_sleep_latency");
                         facet.scoreSleepEfficiency = propertiesObject.getDouble("score_sleep_efficiency");
                         facet.scoreAwakenings = propertiesObject.getDouble("score_awakenings");
+
+                        JSONArray sleepTags = sleepObject.getJSONArray("tags");
+                        List<String> sleepTagsData = new ArrayList<String>();
+                        for (int i = 0; i < sleepTags.size(); i++) {
+                            sleepTagsData.add(sleepTags.getString(i));
+                        }
+                        facet.setSleepTags(sleepTagsData);
+
+
+                        JSONObject timeValueTracksObject = sleepObject.getJSONObject("time_value_tracks");
+
+                        JSONArray sleepCycles = timeValueTracksObject.getJSONObject("sleep_cycles").getJSONArray("items");
+                        List<Pair<Long,Double>> sleepCycleData = new ArrayList<Pair<Long,Double>>();
+                        for (int i = 0; i < sleepCycles.size(); i++) {
+                            sleepCycleData.add(new Pair<Long, Double>(getUTCMillis(sleepCycles.getJSONArray(i).getDouble(0), timezone), sleepCycles.getJSONArray(i).getDouble(1)));
+                        }
+                        facet.setSleepCycles(sleepCycleData);
+
+                        JSONArray heartRateCurve = timeValueTracksObject.getJSONObject("heart_rate_curve").getJSONArray("items");
+                        List<Pair<Long,Double>> heartRateCurveData = new ArrayList<Pair<Long,Double>>();
+                        for (int i = 0; i < heartRateCurve.size(); i++) {
+                            heartRateCurveData.add(new Pair<Long, Double>(getUTCMillis(heartRateCurve.getJSONArray(i).getDouble(0), timezone), heartRateCurve.getJSONArray(i).getDouble(1)));
+                        }
+                        facet.setHeartRateCurve(heartRateCurveData);
+
+                        JSONArray sleepStages = timeValueTracksObject.getJSONObject("sleep_stages").getJSONArray("items");
+                        List<Pair<Long,Integer>> sleepStagesData = new ArrayList<Pair<Long, Integer>>();
+                        for (int i = 0 ; i < sleepStages.size(); i++) {
+                            sleepStagesData.add(new Pair<Long, Integer>(getUTCMillis(sleepStages.getJSONArray(i).getDouble(0), timezone), sleepStages.getJSONArray(i).getInt(1)));
+                        }
+                        facet.setSleepStages(sleepStagesData);
+
+                        JSONArray snoringEpisodes = timeValueTracksObject.getJSONObject("snoring_episodes").getJSONArray("items");
+                        List<Pair<Long,Double>> snoringEpisodesData = new ArrayList<Pair<Long, Double>>();
+                        for (int i = 0; i < snoringEpisodes.size(); i++) {
+                            snoringEpisodesData.add(new Pair<Long,Double>(getUTCMillis(snoringEpisodes.getJSONArray(i).getDouble(0), timezone), snoringEpisodes.getJSONArray(i).getDouble(1)));
+                        }
+                        facet.setSnoringEpisodes(snoringEpisodesData);
+
+
 
                         return facet;
                     }
--- fluxtream-connectors/src/main/java/org/fluxtream/connectors/beddit/SleepFacet.java
+++ fluxtream-connectors/src/main/java/org/fluxtream/connectors/beddit/SleepFacet.java
@@ -1,19 +1,35 @@
 package org.fluxtream.connectors.beddit;
 
 
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+import com.google.gdata.util.common.base.Pair;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.type.TypeFactory;
 import org.fluxtream.core.connectors.annotations.ObjectTypeSpec;
 import org.fluxtream.core.domain.AbstractFacet;
 
 import javax.persistence.Entity;
+import javax.persistence.Lob;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 @Entity(name="Facet_BedditSleep")
 @ObjectTypeSpec(name = "sleep", value = 1, prettyname = "Sleep Logs")
 public class SleepFacet extends AbstractFacet {
 
+    static final int STATE_AWAY_FROM_BED = 65;
+    static final int STATE_ASLEEP = 83;
+    static final int STATE_AWAKE = 87;
+    static final int STATE_MEASUREMENT_GAP = 71;
+
+
     public SleepFacet(){super();}
     public SleepFacet(long apiKeyId){super(apiKeyId);}
 
-    //time the data corresponding to this facet was updated on beddit's server
+    //time the data corresponding to this facet was updated on beddit's server. Stored as seconds double to match the API format
     public double updatedTime;
 
     //Target sleep amount for user in seconds
@@ -53,8 +69,146 @@ public class SleepFacet extends AbstractFacet {
     public double scoreSleepEfficiency; //time asleep vs time in bed
     public double scoreAwakenings;
 
+    @Lob
+    public String sleepTags;
+
+    @Lob
+    public String sleepCyclesData;
+
+    @Lob
+    public String heartRateCurveData;
+
+    @Lob
+    public String sleepStagesData;
+
+    @Lob
+    public String snoringEpisodesData;
+
+    public void setSleepTags(List<String> sleepTags){
+        try{
+            this.sleepTags = new ObjectMapper().writeValueAsString(sleepTags);
+        }
+
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public List<String> getSleepTags(){
+        try {
+            return  new ObjectMapper().readValue(sleepTags, TypeFactory.defaultInstance().constructCollectionType(List.class, String.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public void setSleepCycles(List<Pair<Long,Double>> sleepCycles){
+        try{
+            this.sleepCyclesData = new ObjectMapper().writeValueAsString(sleepCycles);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Pair<Long,Double>> getSleepCycles(){
+        try {
+            List<Pair<Long,Double>> list = new LinkedList<Pair<Long,Double>>();
+            JsonNode rootNode = new ObjectMapper().readTree(this.sleepCyclesData);
+            for (Iterator<JsonNode> i = rootNode.getElements(); i.hasNext();){
+                JsonNode node = i.next();
+                list.add(new Pair<Long,Double>(node.get("first").asLong(),node.get("second").asDouble()));
+            }
+            return list;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void setHeartRateCurve(List<Pair<Long,Double>> heartRateCurve) {
+        try {
+            this.heartRateCurveData = new ObjectMapper().writeValueAsString(heartRateCurve);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Pair<Long,Double>> getHeartRateCurve() {
+        try{
+            List<Pair<Long,Double>> list = new LinkedList<Pair<Long,Double>>();
+            JsonNode rootNode = new ObjectMapper().readTree(this.heartRateCurveData);
+            for (Iterator<JsonNode> i = rootNode.getElements(); i.hasNext();){
+                JsonNode node = i.next();
+                list.add(new Pair<Long,Double>(node.get("first").asLong(),node.get("second").asDouble()));
+            }
+            return list;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void setSleepStages(List<Pair<Long,Integer>> sleepStages) {
+        try {
+            this.sleepStagesData = new ObjectMapper().writeValueAsString(sleepStages);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Pair<Long,Integer>> getSleepStages() {
+        try{
+            List<Pair<Long,Integer>> list = new LinkedList<Pair<Long,Integer>>();
+            JsonNode rootNode = new ObjectMapper().readTree(this.sleepStagesData);
+            for (Iterator<JsonNode> i = rootNode.getElements(); i.hasNext();){
+                JsonNode node = i.next();
+                list.add(new Pair<Long,Integer>(node.get("first").asLong(),node.get("second").asInt()));
+            }
+            return list;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void setSnoringEpisodes(List<Pair<Long,Double>> snoringEpisodes) {
+        try {
+            this.snoringEpisodesData = new ObjectMapper().writeValueAsString(snoringEpisodes);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Pair<Long,Double>> getSnoringEpisodes() {
+        try{
+            List<Pair<Long,Double>> list = new LinkedList<Pair<Long,Double>>();
+            JsonNode rootNode = new ObjectMapper().readTree(this.snoringEpisodesData);
+            for (Iterator<JsonNode> i = rootNode.getElements(); i.hasNext();){
+                JsonNode node = i.next();
+                list.add(new Pair<Long,Double>(node.get("first").asLong(),node.get("second").asDouble()));
+            }
+            return list;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
     @Override
     protected void makeFullTextIndexable() {
 
     }
 }
+
