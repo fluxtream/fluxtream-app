88bab0eba2a3b6112ebd197228e10c3877577389
jloutsenhizer <nano351@gmail.com>
2014-12-02 22:02:07 -0500

    -Removed test that made all widgets be sandboxed

--- fluxtream-web/src/main/webapp/js/applications/calendar/tabs/dashboards/DashboardsTab.js
+++ fluxtream-web/src/main/webapp/js/applications/calendar/tabs/dashboards/DashboardsTab.js
@@ -126,7 +126,7 @@ define(["core/Tab",
 
    function loadWidget(widgetInfo) {
        var that = this;
-       if (window.location.host == getWidgetHostName(widgetInfo) && false) {
+       if (window.location.host == getWidgetHostName(widgetInfo)) {
            require([widgetInfo.manifest.WidgetRepositoryURL + "/"
                         + widgetInfo.manifest.WidgetName + "/"
                         + widgetInfo.manifest.WidgetName + ".js"],
