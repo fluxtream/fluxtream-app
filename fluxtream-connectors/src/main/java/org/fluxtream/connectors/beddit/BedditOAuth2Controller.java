2eb9b54b2ab4f4d630eb5b70cebd6f4ddd50f5a3
Candide Kemmler <ckemmler@gmail.com>
2015-02-10 17:35:53 +0100

    WIP: using form-enc'd POST params @ authorize step
    getting a 500 now

--- fluxtream-connectors/src/main/java/org/fluxtream/connectors/beddit/BedditOAuth2Controller.java
+++ fluxtream-connectors/src/main/java/org/fluxtream/connectors/beddit/BedditOAuth2Controller.java
@@ -1,5 +1,19 @@
 package org.fluxtream.connectors.beddit;
 
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.fluxtream.core.Configuration;
 import org.fluxtream.core.auth.AuthHelper;
 import org.fluxtream.core.connectors.Connector;
@@ -7,6 +21,9 @@ import org.fluxtream.core.domain.ApiKey;
 import org.fluxtream.core.domain.Guest;
 import org.fluxtream.core.services.GuestService;
 import org.fluxtream.core.services.JPADaoService;
+import org.fluxtream.core.utils.HttpUtils;
+import org.fluxtream.core.utils.UnexpectedHttpResponseCodeException;
+import org.joda.time.DateTimeConstants;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.model.Token;
 import org.scribe.model.Verifier;
@@ -26,7 +43,10 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.GeneralSecurityException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
+import java.util.*;
 
 /**
  * Created by candide on 09/02/15.
@@ -42,13 +62,13 @@ public class BedditOAuth2Controller {
     private static final Token EMPTY_TOKEN = null;
 
     @Autowired
-    Configuration env;
-
-    @Autowired
     GuestService guestService;
 
     @Autowired
     JPADaoService jpaDaoService;
+
+    @Autowired
+    Configuration env;
 
     // this seems to be needed in order to be able to access the beddit servers from — at least – my local dev machine
     static void trustAllSSLCertificates() {
@@ -105,29 +125,39 @@ public class BedditOAuth2Controller {
     }
 
     @RequestMapping(value = "/upgradeToken")
-    public String upgradeToken(HttpServletRequest request) throws IOException {
+    public String upgradeToken(HttpServletRequest request) throws IOException, UnexpectedHttpResponseCodeException, NoSuchAlgorithmException, KeyManagementException {
         final String code = request.getParameter("code");
-        Verifier verifier = new Verifier(code);
-        OAuthService service = (OAuthService) request.getSession().getAttribute(BEDDIT_SERVICE);
+        Map<String,String> parameters = new HashMap<String,String>();
+        parameters.put("grant_type", "authorization_code");
+        parameters.put("code", code);
+        parameters.put("client_id", env.get("bedditConsumerKey"));
+        parameters.put("client_secret", env.get("bedditConsumerSecret"));
+        final String json = fetch("https://cloudapi.beddit.com/api/v1/auth/authorize", parameters);
+        JSONObject token = JSONObject.fromObject(json);
 
-        Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
+        final String refresh_token = token.getString("refresh_token");
 
-        final String token = accessToken.getToken();
+        // Create the entry for this new apiKey in the apiKey table and populate
+        // ApiKeyAttributes with all of the keys fro oauth.properties needed for
+        // subsequent update of this connector instance.
+        ApiKey apiKey;
 
         Guest guest = AuthHelper.getGuest();
-
         final Connector connector = Connector.getConnector("beddit");
 
-        ApiKey apiKey;
-        if (request.getSession().getAttribute(BEDDIT_RENEWTOKEN_APIKEYID) != null) {
-            final String apiKeyIdString = (String) request.getSession().getAttribute(BEDDIT_RENEWTOKEN_APIKEYID);
-            long apiKeyId = Long.valueOf(apiKeyIdString);
+        final String stateParameter = request.getParameter("state");
+        if (stateParameter !=null&&!StringUtils.isEmpty(stateParameter)) {
+            long apiKeyId = Long.valueOf(stateParameter);
             apiKey = guestService.getApiKey(apiKeyId);
-        } else
+        } else {
             apiKey = guestService.createApiKey(guest.getId(), connector);
+        }
 
         guestService.populateApiKey(apiKey.getId());
-        guestService.setApiKeyAttribute(apiKey, "accessToken", token);
+        guestService.setApiKeyAttribute(apiKey,
+                "accessToken", token.getString("access_token"));
+        guestService.setApiKeyAttribute(apiKey,
+                "refreshToken", refresh_token);
 
         request.getSession().removeAttribute(BEDDIT_REQUEST_TOKEN);
         request.getSession().removeAttribute(BEDDIT_SERVICE);
@@ -137,5 +167,43 @@ public class BedditOAuth2Controller {
         }
         return "redirect:/app/from/beddit";
     }
+
+    public String fetch(String url, Map<String, String> params) throws UnexpectedHttpResponseCodeException, IOException, KeyManagementException, NoSuchAlgorithmException {
+        HttpClient client = env.getHttpClient();
+        if (env.get("development") != null && env.get("development").equals("true")) {
+            HttpHost proxy = new HttpHost("127.0.0.1", 8888, "http");
+            client = HttpUtils.httpClientTrustingAllSSLCerts();
+            client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,proxy);
+        }
+        String content = "";
+        try {
+            HttpPost post = new HttpPost(url);
+
+            Iterator<Map.Entry<String, String>> iterator = params.entrySet().iterator();
+
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(params.size());
+            while (iterator.hasNext()) {
+                Map.Entry<String, String> entry = iterator.next();
+                nameValuePairs.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+            }
+            post.setEntity(new UrlEncodedFormEntity(nameValuePairs, "utf-8"));
+
+            HttpResponse response = client.execute(post);
+
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                content = responseHandler.handleResponse(response);
+            }
+            else {
+                throw new UnexpectedHttpResponseCodeException(response.getStatusLine().getStatusCode(),
+                        response.getStatusLine().getReasonPhrase());
+            }
+        }
+        finally {
+            client.getConnectionManager().shutdown();
+        }
+        return content;
+    }
+
 
 }
