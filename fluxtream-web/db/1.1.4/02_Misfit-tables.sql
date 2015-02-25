ce551f58e407e15d461faef5b257492f66f219c4
Candide Kemmler <ckemmler@gmail.com>
2015-02-25 17:07:10 +0100

    added missing table for Misfit sleep

--- fluxtream-web/db/1.1.4/02_Misfit-tables.sql
+++ fluxtream-web/db/1.1.4/02_Misfit-tables.sql
@@ -62,3 +62,31 @@ CREATE TABLE `Facet_MisfitActivitySummary` (
   KEY `timeUpdated_index` (`timeUpdated`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `Facet_MisfitSleep` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `api` int(11) NOT NULL,
+  `apiKeyId` bigint(20) DEFAULT NULL,
+  `comment` longtext,
+  `end` bigint(20) NOT NULL,
+  `fullTextDescription` longtext,
+  `guestId` bigint(20) NOT NULL,
+  `isEmpty` char(1) NOT NULL,
+  `objectType` int(11) NOT NULL,
+  `start` bigint(20) NOT NULL,
+  `tags` longtext,
+  `timeUpdated` bigint(20) NOT NULL,
+  `autodetected` bit(1) NOT NULL,
+  `misfitId` varchar(255) DEFAULT NULL,
+  `sleepDetails` longtext,
+  `date` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `isEmpty_index` (`isEmpty`),
+  KEY `end_index` (`end`),
+  KEY `start_index` (`start`),
+  KEY `api_index` (`api`),
+  KEY `objectType_index` (`objectType`),
+  KEY `guestId_index` (`guestId`),
+  KEY `apiKey` (`apiKeyId`),
+  KEY `timeUpdated_index` (`timeUpdated`),
+  KEY `date_idx` (`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
